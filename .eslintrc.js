64af75936c8726e9034e999cd4db56da88a14453
Wooseong Kim <innocarpe@gmail.com>
2023-08-27 23:59:28 +0900

    .eslintrc.js extends 영역에 적용할 플러그인 목록 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   overrides: [
     {
