2a1e9aa7f958e20a1088e4deeae4b3a8c54b1c5e
heo min <min@weffwd.com>
2023-08-29 02:46:59 +0900

    chore : Dockerfile을 추가합니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import Providers from '@/lib/Providers';
 import './normalize.css';
 import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
   title: 'react world',
   description: 'react world by min',
 };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <Providers>
