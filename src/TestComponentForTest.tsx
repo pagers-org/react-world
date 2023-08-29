20fea7deda3818412628ee622ec88e86dbb4f688
steven-yn <ssungyeon94@gmail.com>
2023-08-29 17:16:49 +0900

    test: lint-staged 테스트

--- src/TestComponentForTest.tsx
+++ src/TestComponentForTest.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const TestComponentForTest = () => {
-  return <div>yoon0cean starterKit</div>;
+  return (
+    <div>
+      yoon0cean starterKit
+      <p>test commit</p>
+    </div>
+  );
 };
 
 export default TestComponentForTest;
