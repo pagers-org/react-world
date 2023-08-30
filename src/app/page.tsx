ca23e5c9ae996ceb7707717a302b553bb8ac2f3c
cham0287 <crtmt97@gmail.com>
2023-08-30 23:39:21 +0900

    Chore: prettier trivago sort imports setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import React from "react";
-
 export default function Home() {
-  function fn(n: number) {
-    if (n > 5) {
-      return true;
-    } else {
-      return false;
-    }
-    return true;
-  }
-
   return <h1>Hello Next!</h1>;
 }
