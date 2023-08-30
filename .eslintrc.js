bed1f70647c78c9fcc618a864d24264ad3aab980
merge <fpel8800@gmail.com>
2023-08-31 04:37:24 +0900

    chore: next 의존성 설정
    - 설치한 tsconfig/next를 extends 하지 못해 임시로 제거
    - nextjs에 맞게 lint-staged 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "no-duplicate-imports": "error",
-        "no-constructor-return": "error",
-        "no-constant-binary-expression": "error",
-        "array-callback-return": "error",
-        "no-promise-executor-return": "error",
-        "no-self-compare": "error",
-        "no-template-curly-in-string": "error",
-        "no-undef": "off"
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-duplicate-imports": "error",
+    "no-constructor-return": "error",
+    "no-constant-binary-expression": "error",
+    "array-callback-return": "error",
+    "no-promise-executor-return": "error",
+    "no-self-compare": "error",
+    "no-template-curly-in-string": "error",
+    "no-undef": "off",
+  },
+};
