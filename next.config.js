462bcd75a13bb83a1130912cadd950533469b8e2
hyeon9782 <hyeon9782@gmail.com>
2023-08-31 20:44:12 +0900

    chore: vanilla extract 환경 설정

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const {
+    createVanillaExtractPlugin
+  } = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withVanillaExtract(nextConfig);
