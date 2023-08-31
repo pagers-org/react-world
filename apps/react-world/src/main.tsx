934bc24da016428cd8d56688d7e7af8e56eada54
Wooseong Kim <innocarpe@gmail.com>
2023-09-01 00:15:18 +0900

    feat(home): main.css 파일 임포트

--- apps/react-world/src/main.tsx
+++ apps/react-world/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HomePage } from './pages/home.tsx';
+import '/styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
