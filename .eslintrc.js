cbca108d6fc76bcb6d5816fc348e75048c781136
Wooseong Kim <2222333+innocarpe@users.noreply.github.com>
2023-08-31 18:45:01 +0900

    Chore: 프로젝트 기초 환경 설정을 완료합니다. (#10)
    * yarn 을 통해 prettier 설치
    
    * yarn 통해 eslint 설치
    
    * AirBnb 스타일의 eslint 를 적용하기 위한 의존성 설치
    
    * prettier 의 설정 중 eslint 의 설정과 충돌이 나는 설정들을 비활성화하기 위해 eslint-config-pretteir 의존성 설치
    
    * prettier 규칙을 생성하는 eslint 플러그인 의존성 추가
    
    * React eslint 플러그인 의존성 추가
    
    * React Hooks eslint 플러그인 의존성 추가
    
    * JSX 문법도 정적분석을 할 수 있도록 하는 eslint 모듈 의존성 추가
    
    * Typescript에서 ESLint를 사용할 수 있도록 해주는 의존성 추가
    
    * TypeScript 린팅(linting) 규칙을 실행할 수 있는 ESlint 플러그인 의존성 추가
    
    * .prettierrc 작성
    
    - 주석을 편하게 쓰기 위해 .js 로 작성
    
    * VS Code 설정 파일 추가
    
    * eslint --init 을 통해 .eslintrc.js 최초 세팅
    
    * .eslintrc.js 포맷 변환
    
    * .eslintrc.js extends 영역에 적용할 플러그인 목록 추가
    
    * package.json 에 lint 스크립트 추가
    
    * 타입스크립트 의존성 추가
    
    * Typescript 를 좀 더 안정적인 5.1.6 으로 다운그레이드
    
    * react 와 react-dom 의존성 설치
    
    * .eslintrc.js 에 React 버전 detect 설정 추가
    
    * yarn 버전을 berry 로 변경
    
    (cherry picked from commit f8be0988bc6e458463cb4a0d2c757bf2c872e57f)
    
    * PR 템플릿 보완
    
    * VS Code 설정 추가
    
    * Yarn zero-install 를 위한 gitignore 설정 변경
    
    * Storybook gitignore 설정
    
    * yarn install 를 통한 의존성 추가
    
    * .yarnrc.yml 에서 노드링커를 pnp 로 설정
    
    * pnp 관련 파일 추가
    
    * yarn 에디터(VS Code) 셋업
    
    - yarn dlx @yarnpkg/sdks vscode 실행
    
    * @types로 시작하는 별도의 타입 정의를 설치해줘야 하는 작업을 자동화 해주는 플러그인 설치
    
    - yarn plugin import typescript
    
    * .eslintrc.js extends 내 순서 변경
    
    * package.json 에 contributors 추가
    
    * .prettierignore 추가
    
    * 기본 tsconfig.json 추가
    
    * tsconfig.json 옵션 수정

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 문자열은 작은 따옴표로 통일
+  singleQuote: true,
+  // 코드 마지막에 세미콜른이 자동 생성
+  semi: true,
+  // 탭의 사용을 금하고 스페이스바 사용으로 대체
+  useTabs: false,
+  // 들여쓰기 너비는 2칸
+  tabWidth: 2,
+  // 객체나 배열 키:값 뒤에 콤마 생성
+  trailingComma: 'all',
+  // 코드 한줄이 maximum 80칸
+  printWidth: 80,
+  // 마지막 줄 개행 처리
+  endOfLine: 'auto',
+  // 화살표 함수가 하나의 매개변수를 받을 때 괄호 생략
+  arrowParens: 'avoid',
+};
