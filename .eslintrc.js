dd414b16f1fec359f0c01d33e08c9abae30c5312
kimseongchan-kr <72206135+kimseongchan-kr@users.noreply.github.com>
2023-08-31 22:32:46 +0900

    Chore: 프로젝트 기초 환경 설정을 완료합니다. (#29)
    * chore: .gitignore 수정
    
    .gitignore
    - .idea 폴더가 git에 올라가지 않도록 .gitignore 파일에 추가
    
    * chore: package.json author 수정
    
    * chore: ESLint 설정
    
    - ESLint 설정은 프로젝트를 진행하면서 지속적으로 업데이트 예정
    
    * chore: Prettier 설정
    
    * chore: husky 및 lint-staged 설정
    
    * chore: typescript 설정
    
    https://github.com/tsconfig/bases/blob/main/bases/next.json
    - extends로 next-base 설정
    - 절대 경로 설정
    
    * chore: msw 설정
    
    * chore: next 의존성 설정
    
    - 설치한 tsconfig/next를 extends 하지 못해 임시로 제거
    - nextjs에 맞게 lint-staged 수정
    
    * chore: storybook 설정
    
    - jackspeak 라는 라이브러리가 업데이트를 진행하면서 yarn 1을 지원하지 않아 스토리북 설치 시 오류 발생 -> yarn 3로 업데이트
    
    * chore: cypress 의존성 추가

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next",
+    "plugin:storybook/recommended"
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-duplicate-imports": "error",
+    "no-constructor-return": "error",
+    "no-constant-binary-expression": "error",
+    "array-callback-return": "error",
+    "no-promise-executor-return": "error",
+    "no-self-compare": "error",
+    "no-template-curly-in-string": "error",
+    "no-undef": "off",
+  },
+};
