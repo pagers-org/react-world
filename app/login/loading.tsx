b4454082e683a372338ac6a357e4ea3d6f7772fa
hyeon9782 <hyeon9782@gmail.com>
2023-08-31 21:06:41 +0900

    feat: login, register page 생성

---
+++ app/login/loading.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const LoginLoading = () => {
+    return <div>Login Loading...</div>
+}
+
+export default LoginLoading;
---
+++ app/register/loading.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const RegisterLoading = () => {
+    return <div>Register Loading...</div>
+}
+
+export default RegisterLoading;
