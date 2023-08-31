7458768c0ea7779253ea278356125ad1c3c83882
brgndy <brgndy96@gmail.com>
2023-08-31 21:00:49 +0900

    chore : page 단위 나누기

---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoginPage() {
+  return <div>로그인 페이지입니다!</div>;
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RegisterPage() {
+  return <div>회원 등록 페이지입니다!</div>;
+}
