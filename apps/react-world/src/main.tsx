02dbd9f5b3802930bee0aa67c232080dd5a02055
Wooseong Kim <innocarpe@gmail.com>
2023-09-01 00:03:53 +0900

    (home) 기본 파일을 제거하고 HomePage 를 첫 화면으로 띄우도록 함

--- apps/react-world/src/main.tsx
+++ apps/react-world/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { HomePage } from './pages/home.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
 );
