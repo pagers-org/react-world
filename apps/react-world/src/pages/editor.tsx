2a831604c9f2c2d964eafbb27835dae0ca81ad97
Wooseong Kim <innocarpe@gmail.com>
2023-09-03 10:40:42 +0900

    feat(editor): edit 템플릿을 기반으로 EditorPage 작성

---
+++ apps/react-world/src/pages/editor.tsx
@@ -0,0 +1,107 @@
+import { useParams } from 'react-router-dom';
+
+export const EditorPage = () => {
+  // slug 없으면 새 아티클 작성, 존재하면 권한에 따라 기존 아티클 수정
+  const { articleSlug } = useParams();
+
+  console.log({ articleSlug });
+
+  return (
+    <>
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            conduit
+          </a>
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              <a className="nav-link active" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/login">
+                Sign in
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/register">
+                Sign up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="editor-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-10 offset-md-1 col-xs-12">
+              <ul className="error-messages">
+                <li>That title is required</li>
+              </ul>
+
+              <form>
+                <fieldset>
+                  <fieldset className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Article Title"
+                    />
+                  </fieldset>
+                  <fieldset className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="What's this article about?"
+                    />
+                  </fieldset>
+                  <fieldset className="form-group">
+                    <textarea
+                      className="form-control"
+                      rows="8"
+                      placeholder="Write your article (in markdown)"
+                    ></textarea>
+                  </fieldset>
+                  <fieldset className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter tags"
+                    />
+                    <div className="tag-list">
+                      <span className="tag-default tag-pill">
+                        {' '}
+                        <i className="ion-close-round"></i> tag{' '}
+                      </span>
+                    </div>
+                  </fieldset>
+                  <button
+                    className="btn btn-lg pull-xs-right btn-primary"
+                    type="button"
+                  >
+                    Publish Article
+                  </button>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <div className="container">
+          <a href="/" className="logo-font">
+            conduit
+          </a>
+          <span className="attribution">
+            An interactive learning project from{' '}
+            <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
+            licensed under MIT.
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+};
