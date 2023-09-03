5a5d575d0fc78e7681ac0b7e1c7465238063b967
seulgi <sgsg9447@gmail.com>
2023-09-03 22:34:18 +0900

    setting: design-system package.json 라이브러리 설치

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+//TODO : build설정 수정 필요
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/main.tsx",
+      name: "main",
+      formats: ["es", "cjs"],
+      fileName: "index",
+    },
+  },
+});
