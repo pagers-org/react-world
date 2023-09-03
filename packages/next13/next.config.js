b9b2cb8a3326ce654448f166de886447b584d1c2
mos <ysg01129@naver.com>
2023-09-03 20:01:57 +0900

    chore: Improve initial setting
    - monorepo eslint setting
    - next13 setting
    - typescript setting

---
+++ packages/next13/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        typedRoutes: true,
+    },
+}
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig
