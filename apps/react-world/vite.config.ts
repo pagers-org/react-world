039952c24e278ca426d0b9f1f96baba6c2d6b2e3
Wooseong Kim <carpe@29cm.co.kr>
2023-09-05 00:17:25 +0900

    chore(app) dev 도메인을 /etc/hosts 에 설정하고 사용할 수 있도록 vite.config 수정

--- apps/react-world/vite.config.ts
+++ apps/react-world/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // 외부 접근을 허용합니다.
+    cors: true,
+  },
   plugins: [react()],
 });
