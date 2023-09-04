f0594b23508d55c1edbf219b36b0fb86f563299c
brgndy <brgndy96@gmail.com>
2023-09-04 14:37:10 +0900

    chore : 메인 page.tsx를 작성합니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function MainPage() {
-  return <div>메인 페이지에요!</div>;
+import HomeBanner from "@/components/composables/HomeBanner/HomeBanner";
+import ArticleList from "@/components/Articles/ArticleList";
+import { getArticles } from "@/services/getArticles";
+
+export default async function MainPage() {
+  const allArticles = await getArticles();
+  let articles = allArticles.articles;
+  let pageCount = Math.ceil(allArticles.articlesCount / 9);
+
+  return (
+    <>
+      <HomeBanner />
+      <ArticleList articles={articles} pageCount={pageCount} />
+    </>
+  );
 }
