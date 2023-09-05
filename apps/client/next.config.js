9325480bf056c63bd5c45da303667f9f08d46f22
Jaehyeok <milkboy2564@naver.com>
2023-09-05 21:02:05 +0900

    chore: vanila extract 의존성 설치

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
 
-module.exports = nextConfig
+const nextConfig = {};
+
+module.exports = withVanillaExtract(nextConfig);
