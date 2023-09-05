01663d8d568632cd90b0d419a8a49d6120224761
Jaehyeok <milkboy2564@naver.com>
2023-09-05 21:21:05 +0900

    feature: GlobalStyle 설정 후 Root Layout에 주입시키기

---
+++ apps/client/app/global.css.ts
@@ -0,0 +1,11 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('html, body', {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    border: 0,
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+});
