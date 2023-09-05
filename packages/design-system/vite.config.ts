9f6b50534e82b8920ae6b2a81ce13784266b742d
seulgi <sgsg9447@gmail.com>
2023-09-05 18:23:47 +0900

    setting: design-system 절대경로 변경

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 //TODO : build설정 수정 필요
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
       entry: "src/main.tsx",
