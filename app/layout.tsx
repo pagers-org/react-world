b4a812da2ba95badd78550cf744a03d6ad0a9fe7
Jiwon Han <nomad.jiwhan@gmail.com>
2023-09-06 23:13:25 +0900

    chore: scripts 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
+import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
 
 import 'styles/normalize.css';
 import 'styles/tailwind.css';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export const metadata = {
+  title: 'Real World',
+  description: 'real world project',
+};
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>{children}</body>
     </html>
   );
