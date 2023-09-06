3c37cb1178d165f3ad92d4d268c2eaf59a26adc4
TAEHEON <brgndy96@gmail.com>
2023-09-07 08:29:21 +0900

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function MainPage() {
-  return <div>메인 페이지에요!</div>;
+import HomeBanner from "@/components/composables/HomeBanner/HomeBanner";
+import ArticleList from "@/components/Articles/ArticleList";
+import { getArticles } from "@/services/getArticles";
+import { getPopularTagLists } from "@/services/getPopularTagLists";
+
+export default async function MainPage() {
+  const allArticles = await getArticles();
+  const articles = allArticles.articles;
+  const initialPageCount = Math.ceil(allArticles.articlesCount / 9);
+  const tagLists = await getPopularTagLists();
+
+  return (
+    <>
+      <HomeBanner />
+      <ArticleList
+        articles={articles}
+        initialPageCount={initialPageCount}
+        tagLists={tagLists}
+      />
+    </>
+  );
 }
