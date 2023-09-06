eaf31cadd787671ac61bd037803c28e0e9e98dd8
Jaehyeok <milkboy2564@naver.com>
2023-09-06 18:02:56 +0900

    feature: Main Page 및 Layout 관련 컴포넌트 추가

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,6 +1,11 @@
 import './global.css.ts';
+import '../../../styles/main.css';
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import Header from '@/components/layout/header';
+import Footer from '@/components/layout/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +21,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Header />
+                {children}
+                <Footer />
+            </body>
         </html>
     );
 }
