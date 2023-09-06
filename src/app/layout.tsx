72452d17d6e06bd30328e2479a076be7c7790394
TAEHEON <brgndy96@gmail.com>
2023-09-07 08:22:47 +0900

    chore : 충돌 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,41 @@
+import { getThemeCookieValue } from "@/utils/getThemeCookieValue";
+import { generateThemeScript } from "@/utils/generateThemeScript";
+import Card from "@/components/composables/Card/Card";
+import Header from "@/components/Header/Header";
+import ContentCard from "@/components/composables/Card/ContentCard";
+import Footer from "@/components/composables/Footer/Footer";
+import { getUserInfo } from "@/utils/getUserInfo";
+import Sign from "@/components/Header/Sign";
+import UserProfile from "@/components/User/UserProfile";
+import "./global.css";
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentTheme = getThemeCookieValue();
+  const themeInitializerScript = generateThemeScript(currentTheme);
+  const userInfo = await getUserInfo();
+  const { user, loggedInSuccess } = userInfo;
+
   return (
     <html lang="en">
       <head />
-      <body>
-        <script></script>
+      <body suppressHydrationWarning={true}>
+        <script
+          dangerouslySetInnerHTML={{ __html: themeInitializerScript }}
+        ></script>
 
-        {children}
+        <Card>
+          <Header
+            currentTheme={currentTheme}
+            loggedInSuccess={loggedInSuccess}
+            userInfo={loggedInSuccess && user}
+          />
+          <ContentCard>{children}</ContentCard>
+          <Footer />
+        </Card>
+        <div id="toast-root"></div>
       </body>
     </html>
   );
