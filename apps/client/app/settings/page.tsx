5e2fe844353bc80b1cd4931e171155db20e0b78c
Jaehyeok <milkboy2564@naver.com>
2023-09-06 18:30:36 +0900

    feature: 모든 페이지 UI 추가 - 추가한 UI에 기능과 스타일링(vanila extract)을 새롭게 적용하는 방식으로 리팩터링 진행 예정

--- apps/client/app/settings/page.tsx
+++ apps/client/app/settings/page.tsx
@@ -1,3 +1,64 @@
 export default function Page() {
-    return <div>Setting Page</div>;
+    return (
+        <div className="settings-page">
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-xs-12">
+                        <h1 className="text-xs-center">Your Settings</h1>
+
+                        <ul className="error-messages">
+                            <li>That name is required</li>
+                        </ul>
+
+                        <form>
+                            <fieldset>
+                                <fieldset className="form-group">
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="URL of profile picture"
+                                    />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <input
+                                        className="form-control form-control-lg"
+                                        type="text"
+                                        placeholder="Your Name"
+                                    />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <textarea
+                                        className="form-control form-control-lg"
+                                        rows={8}
+                                        placeholder="Short bio about you"
+                                    ></textarea>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <input
+                                        className="form-control form-control-lg"
+                                        type="text"
+                                        placeholder="Email"
+                                    />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <input
+                                        className="form-control form-control-lg"
+                                        type="password"
+                                        placeholder="New Password"
+                                    />
+                                </fieldset>
+                                <button className="btn btn-lg btn-primary pull-xs-right">
+                                    Update Settings
+                                </button>
+                            </fieldset>
+                        </form>
+                        <hr />
+                        <button className="btn btn-outline-danger">
+                            Or click here to logout.
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
