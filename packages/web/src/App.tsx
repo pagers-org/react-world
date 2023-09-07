0a5c00e4bed4a71619cc21709193e9f8f8b0e537
aryang <aryang@whatap.io>
2023-09-07 21:04:56 +0900

    feat(App): globalStyle 추가 및 route, theme 연결
    (cherry picked from commit 5169a358b7f1fb73d2954e1e17edeb801fecf679)

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Global, ThemeProvider } from "@emotion/react";
+import { color } from "@miso/common";
+
+import { globalStyles } from "styles/globalStyles";
+import Routes from "pages/routes";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={color}>
+      <Global styles={globalStyles} />
+      <Routes />
+    </ThemeProvider>
+  );
 };
 
 export default App;
