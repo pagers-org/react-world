ee2a626f795e0cd37c5ed5bdfc87c90d1d7c981b
aryang <aryang@whatap.io>
2023-09-07 20:54:18 +0900

    feat(config): ts, vite 절대 경로 설정 추가
    (cherry picked from commit 618aa0bd8419112e49d90dddc31ba4008a6b47bd)

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      src: path.resolve("src/"),
+    },
+  },
   plugins: [react()],
-})
+});
