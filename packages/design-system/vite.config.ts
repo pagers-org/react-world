46a0cbdf3e3c85563f25765be72e6ddc5fdd2508
seulgi <sgsg9447@gmail.com>
2023-09-07 21:12:47 +0900

    setting: vanilla-extract-plugin 설치

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 //TODO : build설정 수정 필요
 export default defineConfig({
   base: "./",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
   build: {
     lib: {
       entry: "src/main.tsx",
