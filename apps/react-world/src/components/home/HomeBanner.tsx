0560aa37e7d72747018631aed96a3d5ebc2da7ad
Wooseong Kim <innocarpe@gmail.com>
2023-09-08 01:08:28 +0900

    feat(home): HomeBanner 를 styled 기반으로 변경

--- apps/react-world/src/components/home/HomeBanner.tsx
+++ apps/react-world/src/components/home/HomeBanner.tsx
@@ -1,12 +1,36 @@
+import styled from '@emotion/styled';
+import { Banner } from '../shared/banner/Banner';
+import { BannerTitle } from '../shared/banner/BannerTitle';
+
 export const HomeBanner: React.FC = () => {
   return (
-    <div className="home-page">
-      <div className="banner">
-        <div className="container">
-          <h1 className="logo-font">conduit</h1>
-          <p>A place to share your knowledge.</p>
-        </div>
-      </div>
-    </div>
+    <HomeBannerContainer>
+      <HomeBannerTitle>conduit</HomeBannerTitle>
+      <HomeBannerText>A place to share your knowledge.</HomeBannerText>
+    </HomeBannerContainer>
   );
 };
+
+const HomeBannerContainer = styled(Banner)`
+  background: #5cb85c;
+  box-shadow:
+    inset 0 8px 8px -8px rgba(0, 0, 0, 0.3),
+    inset 0 -8px 8px -8px rgba(0, 0, 0, 0.3);
+  text-align: center;
+`;
+
+const HomeBannerTitle = styled(BannerTitle)`
+  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
+  font-weight: 700;
+  font-size: 3.5rem;
+  padding-bottom: 0.5rem;
+  margin-bottom: 0px;
+  font-family: 'Titillium Web', sans-serif;
+`;
+
+const HomeBannerText = styled.p`
+  color: #fff;
+  font-size: 1.5rem;
+  font-weight: 300;
+  margin-bottom: 0px;
+`;
