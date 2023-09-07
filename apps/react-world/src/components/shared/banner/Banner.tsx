a42d9dde99a0c013182dfa69f80dfe536e837615
Wooseong Kim <innocarpe@gmail.com>
2023-09-08 01:03:24 +0900

    chore(app): 공용 배너 컴포넌트 작성

---
+++ apps/react-world/src/components/shared/banner/Banner.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Banner = styled.div`
+  color: #fff;
+  background: #333;
+  padding: 2rem;
+  margin-bottom: 2rem;
+`;
---
+++ apps/react-world/src/components/shared/banner/BannerTitle.tsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const BannerTitle = styled.h1`
+  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
+  margin-bottom: 0px;
+`;
