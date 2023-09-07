bc15bc35c561324c80b9e448ba416391e76478c1
Wooseong Kim <innocarpe@gmail.com>
2023-09-07 21:37:13 +0900

    feat(home): 홈페이지를 React 컴포넌트 기반으로 리팩토링

--- apps/react-world/src/pages/home.tsx
+++ apps/react-world/src/pages/home.tsx
@@ -1,185 +1,68 @@
+import { Navbar } from '../components/NavBar';
+import { HomeBanner } from '../components/home/HomeBanner';
+import { Footer } from '../components/Footer';
+import HomeFeedTab from '../components/home/HomeFeedTab';
+import ArticleTagList from '../components/home/ArticleTagList';
+import ArticlePreview from '../components/home/ArticlePreview';
+import Pagination from '../components/home/Pagination';
+
 export const HomePage = () => {
   return (
     <>
-      <nav className="navbar navbar-light">
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            conduit
-          </a>
-          <ul className="nav navbar-nav pull-xs-right">
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/login">
-                Sign in
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/register">
-                Sign up
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
 
       <div className="home-page">
-        <div className="banner">
-          <div className="container">
-            <h1 className="logo-font">conduit</h1>
-            <p>A place to share your knowledge.</p>
-          </div>
-        </div>
+        <HomeBanner />
 
         <div className="container page">
           <div className="row">
             <div className="col-md-9">
-              <div className="feed-toggle">
-                <ul className="nav nav-pills outline-active">
-                  <li className="nav-item">
-                    <a className="nav-link" href="">
-                      Your Feed
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link active" href="">
-                      Global Feed
-                    </a>
-                  </li>
-                </ul>
-              </div>
+              <HomeFeedTab activeFeed="global_feed" />
 
-              <div className="article-preview">
-                <div className="article-meta">
-                  <a href="/profile/eric-simons">
-                    <img src="http://i.imgur.com/Qr71crq.jpg" />
-                  </a>
-                  <div className="info">
-                    <a href="/profile/eric-simons" className="author">
-                      Eric Simons
-                    </a>
-                    <span className="date">January 20th</span>
-                  </div>
-                  <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                    <i className="ion-heart"></i> 29
-                  </button>
-                </div>
-                <a
-                  href="/article/how-to-build-webapps-that-scale"
-                  className="preview-link"
-                >
-                  <h1>How to build webapps that scale</h1>
-                  <p>This is the description for the post.</p>
-                  <span>Read more...</span>
-                  <ul className="tag-list">
-                    <li className="tag-default tag-pill tag-outline">
-                      realworld
-                    </li>
-                    <li className="tag-default tag-pill tag-outline">
-                      implementations
-                    </li>
-                  </ul>
-                </a>
-              </div>
+              <ArticlePreview
+                authorProfileLink="/profile/eric-simons"
+                authorProfileImageUrl="http://i.imgur.com/Qr71crq.jpg"
+                authorName="Eric Simons"
+                publishDate="January 20th"
+                likeCount={29}
+                articleLink="/article/how-to-build-webapps-that-scale"
+                articleTitle="How to build webapps that scale"
+                articleDescription="This is the description for the post."
+                tags={['realworld', 'implementations']}
+              />
 
-              <div className="article-preview">
-                <div className="article-meta">
-                  <a href="/profile/albert-pai">
-                    <img src="http://i.imgur.com/N4VcUeJ.jpg" />
-                  </a>
-                  <div className="info">
-                    <a href="/profile/albert-pai" className="author">
-                      Albert Pai
-                    </a>
-                    <span className="date">January 20th</span>
-                  </div>
-                  <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                    <i className="ion-heart"></i> 32
-                  </button>
-                </div>
-                <a href="/article/the-song-you" className="preview-link">
-                  <h1>
-                    The song you won&apos;t ever stop singing. No matter how
-                    hard you try.
-                  </h1>
-                  <p>This is the description for the post.</p>
-                  <span>Read more...</span>
-                  <ul className="tag-list">
-                    <li className="tag-default tag-pill tag-outline">
-                      realworld
-                    </li>
-                    <li className="tag-default tag-pill tag-outline">
-                      implementations
-                    </li>
-                  </ul>
-                </a>
-              </div>
+              <ArticlePreview
+                authorProfileLink="/profile/albert-pai"
+                authorProfileImageUrl="http://i.imgur.com/N4VcUeJ.jpg"
+                authorName="Albert Pai"
+                publishDate="January 20th"
+                likeCount={32}
+                articleLink="/article/the-song-you"
+                articleTitle="The song you won't ever stop singing. No matter how hard you try."
+                articleDescription="This is the description for the post."
+                tags={['realworld', 'implementations']}
+              />
 
-              <ul className="pagination">
-                <li className="page-item active">
-                  <a className="page-link" href="">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="">
-                    2
-                  </a>
-                </li>
-              </ul>
+              <Pagination pages={[1, 2]} activePage={1} />
             </div>
 
-            <div className="col-md-3">
-              <div className="sidebar">
-                <p>Popular Tags</p>
-
-                <div className="tag-list">
-                  <a href="" className="tag-pill tag-default">
-                    programming
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    javascript
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    emberjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    angularjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    react
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    mean
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    node
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    rails
-                  </a>
-                </div>
-              </div>
-            </div>
+            <ArticleTagList
+              tags={[
+                'programming',
+                'javascript',
+                'emberjs',
+                'angularjs',
+                'react',
+                'mean',
+                'node',
+                'rails',
+              ]}
+            />
           </div>
         </div>
       </div>
 
-      <footer>
-        <div className="container">
-          <a href="/" className="logo-font">
-            conduit
-          </a>
-          <span className="attribution">
-            An interactive learning project from{' '}
-            <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
-            licensed under MIT.
-          </span>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 };
