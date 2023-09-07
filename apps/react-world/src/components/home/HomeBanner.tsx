dc006d2ca678aa5946111722dc8cb61f460e3c29
Wooseong Kim <innocarpe@gmail.com>
2023-09-07 21:36:02 +0900

    feat(home): 홈페이지에서 사용할 HomeBaner 작성

---
+++ apps/react-world/src/components/home/HomeBanner.tsx
@@ -0,0 +1,12 @@
+export const HomeBanner: React.FC = () => {
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1 className="logo-font">conduit</h1>
+          <p>A place to share your knowledge.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
