28ee1dee6f705517f7d553bfa797df204c719f83
ludacirs <ludacirs@naver.com>
2023-09-07 18:52:01 +0900

    [ui] feat: `Avatar` 구현

---
+++ packages/ui/src/components/Avatar/Avatar.tsx
@@ -0,0 +1,34 @@
+import { forwardRef } from "react";
+
+interface AvatarProps {
+  src?: string;
+  size?: "xxs" | "xs" | "s" | "m" | "big";
+}
+const Avatar = forwardRef<HTMLImageElement, AvatarProps>(
+  (
+    // 이미지 prefix, cdn 으로 캐싱해서 서빙하기
+    { src = "https://api.realworld.io/images/demo-avatar.png", size = "m" },
+    ref
+  ) => {
+    return (
+      <img
+        className={`object-cover w-full h-full rounded-full ${style.size[size]}`}
+        ref={ref}
+        src={src}
+        alt={src}
+      />
+    );
+  }
+);
+
+export default Avatar;
+
+const style = {
+  size: {
+    xxs: "min-w-[26px] min-h-[26px] w-26 h-26",
+    xs: "min-w-[28px] min-h-[28px] w-28 h-28",
+    s: "min-w-[30px] min-h-[30px] w-30 h-30",
+    m: "min-w-[32px] min-h-[32px] w-32 h-32",
+    big: "min-w-[100px] min-h-[100px] w-100 h-100",
+  },
+};
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,6 @@
+import Avatar from "./Avatar/Avatar";
 import Button from "./Button/Button";
+import Tabs from "./Tabs/Tabs";
+import Tag from "./Tag/Tag";
 
-export { Button };
+export { Button, Tag, Tabs, Avatar };
