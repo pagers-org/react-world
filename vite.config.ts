14550c4c6a744c76bba99c963a27f17b4efbc62d
JIHOON LEE <jihoon7705@gmail.com>
2023-09-07 15:14:27 +0900

    chore : vite.config.json 에서 alias @ 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
