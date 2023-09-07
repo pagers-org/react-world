b264fc884015e192ae449b29a322eb93c13cb9c5
ludacirs <ludacirs@naver.com>
2023-09-07 18:55:06 +0900

    feat: `Provider` 구현 및 패키지 추가

--- apps/realworld/next.config.js
+++ apps/realworld/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@packages/ui'],
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
