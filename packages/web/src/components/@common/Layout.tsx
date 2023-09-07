ac73512434854056ac16d76357ea2af72b67ed27
aryang <aryang@whatap.io>
2023-09-07 20:55:25 +0900

    feat(Route): 기본 라우팅 설정 추가
    (cherry picked from commit 1103b3bfe0f072736d65d3072c90d753dceab6b5)

---
+++ packages/web/src/components/@common/Layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return <div>{children}</div>;
+};
+
+export default Layout;
---
+++ packages/web/src/pages/routes.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes as ReactRouterRoutes, Navigate } from "react-router-dom";
+import Layout from "components/@common/Layout";
+import HomePage from "./HomePage";
+
+const Routes = () => {
+  return (
+    <ReactRouterRoutes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Route>
+    </ReactRouterRoutes>
+  );
+};
+
+export default Routes;
