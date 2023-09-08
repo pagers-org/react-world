d37f977c84a7a37c2278f2b13fe77296db8c4712
ding-co <pangoons@naver.com>
2023-09-08 10:35:19 +0900

    feat: Add Users API for fetcher fn, types

---
+++ src/api/users.ts
@@ -0,0 +1,75 @@
+import {
+  CurrentUserPayload,
+  UserLoginPayload,
+  UserRegisterPayload,
+  UserResponse,
+} from '@/types/api/users';
+
+import { COMMON_HEADERS, HTTP_METHOD } from '@/constants/api';
+import { API_BASE_URL } from '@/constants/env';
+
+// Login for existing user
+export const postUserLogin = (
+  payload: UserLoginPayload,
+  options: RequestInit = {},
+): Promise<UserResponse> => {
+  return fetch(`${API_BASE_URL}/users/login`, {
+    ...options,
+    method: HTTP_METHOD.POST,
+    headers: {
+      ...COMMON_HEADERS,
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
+
+// Register a new user
+export const postUserRegister = (
+  payload: UserRegisterPayload,
+  options: RequestInit = {},
+): Promise<UserResponse> => {
+  return fetch(`${API_BASE_URL}/users`, {
+    ...options,
+    method: HTTP_METHOD.POST,
+    headers: {
+      ...COMMON_HEADERS,
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
+
+// Gets the currently logged-in user
+export const getCurrentUser = (
+  options: RequestInit = {},
+): Promise<UserResponse> => {
+  return fetch(`${API_BASE_URL}/user`, {
+    ...options,
+    method: HTTP_METHOD.GET,
+    headers: {
+      ...COMMON_HEADERS,
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
+
+// Updated user information for current user
+export const putCurrentUser = (
+  payload: CurrentUserPayload,
+  options: RequestInit = {},
+): Promise<UserResponse> => {
+  return fetch(`${API_BASE_URL}/user`, {
+    ...options,
+    method: HTTP_METHOD.GET,
+    headers: {
+      ...COMMON_HEADERS,
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
---
+++ src/types/api/users.ts
@@ -0,0 +1,34 @@
+export type UserLoginPayload = {
+  user: {
+    email: string;
+    password: string;
+  };
+};
+
+export type UserRegisterPayload = {
+  user: {
+    username: string;
+    email: string;
+    password: string;
+  };
+};
+
+export type CurrentUserPayload = {
+  user: {
+    email: string;
+    password: string;
+    username: string;
+    bio: string;
+    image: string;
+  };
+};
+
+export type UserResponse = {
+  user: {
+    email: string;
+    token: string;
+    username: string;
+    bio: string;
+    image: string;
+  };
+};
