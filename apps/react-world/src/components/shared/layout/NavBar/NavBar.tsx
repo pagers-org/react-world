3da387133fdf7eaaa2b490b4caa4687940491d70
Wooseong Kim <innocarpe@gmail.com>
2023-09-08 23:44:45 +0900

    feat(app): NavBar 를 폴더 안으로 이동하고, NavBar 를 컴포넌트 기반으로 리팩토링

---
+++ apps/react-world/src/components/shared/layout/NavBar/NavBar.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Container } from '../../Container';
+import { NavItem } from './NavItem';
+import { NavbarBrand } from './NavBarBrand';
+import { NavList } from './NavList';
+
+export const Navbar: React.FC = () => {
+  return (
+    <NavbarContainer>
+      <Container>
+        <NavbarBrand />
+        <NavList>
+          <NavItem href="/" isActive title="Home" />
+          <NavItem href="/login" title="Sign in" />
+          <NavItem href="/register" title="Sign up" />
+        </NavList>
+      </Container>
+    </NavbarContainer>
+  );
+};
+
+const NavbarContainer = styled.nav`
+  display: block;
+  position: relative;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+
+  @media (min-width: 544px) {
+    border-radius: 0.25rem;
+  }
+
+  &::after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+
+  & {
+    .navbar-brand {
+      color: rgba(0, 0, 0, 0.8);
+      &:focus,
+      &:hover {
+        color: rgba(0, 0, 0, 0.8);
+      }
+    }
+
+    .navbar-nav {
+      .nav-link {
+        color: rgba(0, 0, 0, 0.3);
+        &:focus,
+        &:hover {
+          color: rgba(0, 0, 0, 0.6);
+        }
+
+        &.open,
+        &.active,
+        &.open:focus,
+        &.active:focus,
+        &.open:hover,
+        &.active:hover {
+          color: rgba(0, 0, 0, 0.8);
+        }
+      }
+
+      .navbar-divider {
+        background-color: rgba(0, 0, 0, 0.075);
+      }
+    }
+  }
+`;
