f2c289a1b9b7a435773091b069d94069d6dec9b2
ludacirs <ludacirs@naver.com>
2023-09-08 14:22:16 +0900

    feat: `read-more-button` 추가

---
+++ apps/realworld/src/entities/article/index.ts
@@ -0,0 +1,4 @@
+import ArticlePreview from './article-preview/article-preview';
+import ReadMoreButton from './read-more-button/read-more-button';
+
+export { ArticlePreview, ReadMoreButton };
---
+++ apps/realworld/src/entities/article/read-more-button/read-more-button.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface ReadMoreButtonProps {
+  slug: string;
+}
+
+const ReadMoreButton = ({ slug }: ReadMoreButtonProps) => {
+  return (
+    <Link href={`/${slug}`} className="text-gray1200 text-[0.8rem] font-light">
+      Read More..
+    </Link>
+  );
+};
+
+export default ReadMoreButton;
