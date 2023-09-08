3dda0dd27aa269046a55772bdb4a385eb2b21da4
ludacirs <ludacirs@naver.com>
2023-09-08 14:22:29 +0900

    feat: `article-tag-list` 추가

---
+++ apps/realworld/src/entities/tag/article-tag-list/article-tag-list.tsx
@@ -0,0 +1,19 @@
+import { Tag } from '@packages/ui';
+import Link from 'next/link';
+import React from 'react';
+
+interface ArticleTagListProps {
+  tagList: string[];
+  slug: string;
+}
+const ArticleTagList = ({ tagList, slug }: ArticleTagListProps) => {
+  return (
+    <Link className="flex gap-2 cursor-pointer" href={`/${slug}`}>
+      {tagList.map(label => (
+        <Tag key={label} label={label} variant="outlined" />
+      ))}
+    </Link>
+  );
+};
+
+export default ArticleTagList;
---
+++ apps/realworld/src/entities/tag/index.ts
@@ -0,0 +1,4 @@
+import ArticleTagList from './article-tag-list/article-tag-list';
+import ClickToMoveTag from './clikc-to-move-tag/click-to-move-tag';
+
+export { ClickToMoveTag, ArticleTagList };
