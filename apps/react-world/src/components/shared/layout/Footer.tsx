20ef3b6736db7f94b8fbc1ac9f5fff73f583314b
Wooseong Kim <innocarpe@gmail.com>
2023-09-10 02:51:15 +0900

    feat(app): Footer 를 컴포넌트 기반으로 로직 변경

--- apps/react-world/src/components/shared/layout/Footer.tsx
+++ apps/react-world/src/components/shared/layout/Footer.tsx
@@ -1,16 +1,17 @@
+import { Container } from '../Container';
+import { FooterContainer, Logo, AttributedText } from './Footer.styled';
+
 export const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <a href="/" className="logo-font">
-          conduit
-        </a>
-        <span className="attribution">
+    <FooterContainer>
+      <Container>
+        <Logo href="/">conduit</Logo>
+        <AttributedText>
           An interactive learning project from{' '}
           <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
           licensed under MIT.
-        </span>
-      </div>
-    </footer>
+        </AttributedText>
+      </Container>
+    </FooterContainer>
   );
 };
