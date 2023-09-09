4662cc64b9d765bfec18fef66e7db4ab531f25ba
henry <70426440+hyjoong@users.noreply.github.com>
2023-09-09 23:07:01 +0900

    feat: storybook eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:testing-library/react',
     'plugin:jest-dom/recommended',
+    'plugin:storybook/recommended',
   ],
   parser: '@typescript-eslint/p arser',
   parserOptions: {
