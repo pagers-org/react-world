d24c5f555f380eb11d7dba385d3d3b6abce5d092
limgyumin <dev.limgyumin@gmail.com>
2023-09-09 23:20:42 +0900

    chore: main.css, ionicons.css, next/font 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
+import { Titillium_Web } from "next/font/google";
 
 import type { PropsWithChildren } from "react";
 
-import { Providers } from "lib/providers";
+import { Footer } from "components/shared/footer";
+import { Header } from "components/shared/header";
+import { Providers } from "components/shared/providers";
 
-import "styles/normalize.css";
+import "styles/main.css";
+import "styles/ionicons.min.css";
 import "styles/tailwind.css";
 
 export const metadata: Metadata = {
@@ -12,11 +16,21 @@ export const metadata: Metadata = {
   description: "real world project",
 };
 
+const titillium = Titillium_Web({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--titillium-web-font",
+});
+
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
-    <html lang="en">
+    <html lang="en" className={titillium.variable}>
       <Providers>
-        <body>{children}</body>
+        <body>
+          <Header />
+          {children}
+          <Footer />
+        </body>
       </Providers>
     </html>
   );
