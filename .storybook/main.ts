dca8e4ee4abd3db0525b49cc8a1d9dac88759261
henry <70426440+hyjoong@users.noreply.github.com>
2023-09-09 23:06:38 +0900

    feat: storybook configuration files - main.ts - preview.ts

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const path = require('path');
+
+const config: StorybookConfig = {
+  stories: [
+    '../src/components/**/*.mdx',
+    '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  webpackFinal: async (config: any) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    return config;
+  },
+};
+export default config;
