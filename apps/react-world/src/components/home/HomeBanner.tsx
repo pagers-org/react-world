a637e811c0f7dfd89e71f9a0b8615446540cb2b1
Wooseong Kim <innocarpe@gmail.com>
2023-09-09 22:54:44 +0900

    fix(home): HomeBanner 에서 스타일 관련 코드를 HomeBanner.styled.ts 로 추출

--- apps/react-world/src/components/home/HomeBanner.tsx
+++ apps/react-world/src/components/home/HomeBanner.tsx
@@ -1,5 +1,8 @@
-import styled from '@emotion/styled';
-import { Banner, BannerTitle } from '../shared/banner/Banner.styled';
+import {
+  HomeBannerContainer,
+  HomeBannerText,
+  HomeBannerTitle,
+} from './HomeBanner.styled';
 
 export const HomeBanner: React.FC = () => {
   return (
@@ -9,27 +12,3 @@ export const HomeBanner: React.FC = () => {
     </HomeBannerContainer>
   );
 };
-
-const HomeBannerContainer = styled(Banner)`
-  background: #5cb85c;
-  box-shadow:
-    inset 0 8px 8px -8px rgba(0, 0, 0, 0.3),
-    inset 0 -8px 8px -8px rgba(0, 0, 0, 0.3);
-  text-align: center;
-`;
-
-const HomeBannerTitle = styled(BannerTitle)`
-  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
-  font-weight: 700;
-  font-size: 3.5rem;
-  padding-bottom: 0.5rem;
-  margin-bottom: 0px;
-  font-family: 'Titillium Web', sans-serif;
-`;
-
-const HomeBannerText = styled.p`
-  color: #fff;
-  font-size: 1.5rem;
-  font-weight: 300;
-  margin-bottom: 0px;
-`;
