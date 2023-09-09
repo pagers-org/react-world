0d46a8a3adef9911fa95715f51d77c4a753d12bf
heowonho <her2478@gmail.com>
2023-09-09 18:16:10 +0900

    chore: vite vanilla extract 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 export default defineConfig({
   // your configuration options here...
   // https://docs.astro.build/en/reference/configuration-reference/
   integrations: [react()],
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+  },
 });
