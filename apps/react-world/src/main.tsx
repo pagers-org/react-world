6f625e513a7dd377550572a3c3f0b59eb47b5949
Wooseong Kim <innocarpe@gmail.com>
2023-09-09 10:57:48 +0900

    chore(app): 앱에 글로벌 스타일 적용

--- apps/react-world/src/main.tsx
+++ apps/react-world/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Routes } from './routes';
 import '/styles/main.css';
+import { Global } from '@emotion/react';
+import globalStyles from '../styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <Routes />
   </React.StrictMode>,
 );
