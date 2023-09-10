dc418cd1c87ff584d456315731de1a4d2e4d53ae
limgyumin <dev.limgyumin@gmail.com>
2023-09-11 00:14:59 +0900

    feat: Tags 컴포넌트 UI를 tailwind로 재구성

--- components/shared/ui/tags/item.tsx
+++ components/shared/ui/tags/item.tsx
@@ -4,28 +4,40 @@ import { forwardRef } from "react";
 import { clsx } from "lib/clsx";
 import type { Override } from "types/utilities";
 
-type Variant = "default" | "primary" | "success" | "info" | "warning" | "danger";
+const enum Variant {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+type VariantValue = "primary" | "secondary";
 
 type BaseProps<T extends ElementType> = {
   as?: T;
-  variant?: Variant;
-  outline?: boolean;
+  variant?: VariantValue;
 };
 
 type Props<T extends ElementType> = Override<ComponentPropsWithoutRef<T>, BaseProps<T>>;
 
 const Item = <T extends ElementType = "li">(
-  { children, as, className, variant = "default", outline = false, ...rest }: Props<T>,
+  { children, as, className, variant = "primary", ...rest }: Props<T>,
   ref: Ref<any>,
 ) => {
   const Component = as ?? "li";
 
   return (
-    <Component ref={ref} {...rest} className={clsx(className, `tag-pill tag-${variant}`, { "tag-outline": outline })}>
+    <Component ref={ref} {...rest} className={clsx(styles.base, styles.variant[variant], className)}>
       {children}
     </Component>
   );
 };
+
+const styles = {
+  base: "relative mb-[6px] mr-[6px] cursor-pointer rounded-3xl px-3 py-[3px] text-[10px] font-normal tracking-wider last:mr-0",
+  variant: {
+    [Variant.PRIMARY]: "bg-zinc-950 text-white",
+    [Variant.SECONDARY]:
+      "bg-white text-zinc-950 after:absolute after:inset-0 after:rounded-3xl after:border-[1px] after:border-zinc-950 after:content-['']",
+  },
+} as const;
 
 const Default = forwardRef(Item) as typeof Item;
 
