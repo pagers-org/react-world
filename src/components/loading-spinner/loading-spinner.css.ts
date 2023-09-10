bec02778e1a6fa1e80c33d6d6ac23529ef5bbbc5
InSeong-So <goflvhxj2547@gmail.com>
2023-09-10 12:19:10 +0900

    feat: loadding-spinner를 개선합니다.

--- src/components/loading-spinner/loading-spinner.css.ts
+++ src/components/loading-spinner/loading-spinner.css.ts
@@ -1,7 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-export const HomePageTransition = style({});
-
 export const LoadingSpinnerContainer = style({
   width: '100%',
   minHeight: '100%',
