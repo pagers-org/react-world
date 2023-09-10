8ed07fcee7ac0ddf19cd8d652cd1e7c2618e51e8
ding-co <pangoons@naver.com>
2023-09-10 15:09:27 +0900

    feat: Add LoginPageMain UI with Login API

--- src/pageComponents/login/LoginPageMain.tsx
+++ src/pageComponents/login/LoginPageMain.tsx
@@ -1,5 +1,94 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+import { postUserLogin } from '@/api/users';
+
 const LoginPageMain = () => {
-  return <div>로그인 페이지입니다.</div>;
+  const router = useRouter();
+
+  const [form, setForm] = useState<{
+    email: string;
+    password: string;
+  }>({
+    email: '',
+    password: '',
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleSubmit = (event: FormEvent<HTMLElement>) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    postUserLogin({
+      user: {
+        ...form,
+      },
+    }).then((res) => {
+      if (res?.errors) {
+        alert('올바른 이메일과 비밀번호를 입력해주세요!');
+      } else {
+        router.push('/');
+      }
+      setIsLoading(false);
+    });
+  };
+
+  console.log(isLoading);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign in</h1>
+            <p className="text-xs-center">
+              <Link href="/register">Need an account?</Link>
+            </p>
+
+            <form onSubmit={handleSubmit}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={form.password}
+                  onChange={handleChange}
+                />
+              </fieldset>
+              <button
+                className="btn btn-lg btn-primary pull-xs-right"
+                type="submit"
+                disabled={isLoading}
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPageMain;
