724c829038904c9ff6f8d9c9b9b5e6dbf5f8b3a8
ding-co <pangoons@naver.com>
2023-09-10 15:25:34 +0900

    feat: Add RegisterPageMain UI with Register API

--- src/pageComponents/register/RegisterPageMain.tsx
+++ src/pageComponents/register/RegisterPageMain.tsx
@@ -1,5 +1,112 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+import { postUserRegister } from '@/api/users';
+
 const RegisterPageMain = () => {
-  return <div>회원가입 페이지입니다.</div>;
+  const router = useRouter();
+
+  const [form, setForm] = useState<{
+    username: string;
+    email: string;
+    password: string;
+  }>({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleSubmit = (event: FormEvent<HTMLElement>) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+    setIsError(false);
+
+    postUserRegister({
+      user: {
+        ...form,
+      },
+    }).then((res) => {
+      if (res?.errors) {
+        setIsError(true);
+      } else {
+        router.push('/');
+      }
+      setIsLoading(false);
+    });
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign up</h1>
+            <p className="text-xs-center">
+              <Link href="/login">Have an account?</Link>
+            </p>
+
+            {isError && (
+              <ul className="error-messages">
+                <li>That email is already taken</li>
+              </ul>
+            )}
+
+            <form onSubmit={handleSubmit}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={form.username}
+                  onChange={handleChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={form.password}
+                  onChange={handleChange}
+                />
+              </fieldset>
+              <button
+                type="submit"
+                className="btn btn-lg btn-primary pull-xs-right"
+                disabled={isLoading}
+              >
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPageMain;
