4148b19556e8278f14887951cb984d7629910de6
heo min <min@weffwd.com>
2023-09-12 00:30:03 +0900

    feat : articles api의 schema를 추가해줍니다.

---
+++ apps/web/app/(articles)/types/articles.types.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+export interface ArticlesQueryParamsType {
+  tag?: string;
+  author?: string;
+  favorited?: string;
+  offset?: number;
+  limit?: number;
+}
+
+export const ArticleAuthorSchema = z.object({
+  username: z.string(),
+  bio: z.nullable(z.string()),
+  image: z.string(),
+  following: z.boolean(),
+});
+
+export const ArticleSchema = z.object({
+  slug: z.string(),
+  title: z.string(),
+  description: z.string(),
+  body: z.string(),
+  tagList: z.array(z.string()),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  favorited: z.boolean(),
+  favoritesCount: z.number(),
+  author: ArticleAuthorSchema,
+});
+
+export const ArticlesResponseSchema = z.object({
+  articles: z.array(ArticleSchema),
+  articlesCount: z.number(),
+});
