b6388ad89de830a35f5dbcfce0841068fa8a39bb
moseung <tmdah900@naver.com>
2023-09-13 02:48:09 +0900

    feat: 회원가입 페이지 구현

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,12 +1,49 @@
 'use client';
 
-import React, { FormEvent } from 'react';
+import { RegisterPostRequestType } from '@/types/auth';
+import { produce } from 'immer';
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, useState } from 'react';
+
+import { postUserRegister } from '@/api/auth';
 
 const page = () => {
+  const router = useRouter();
+
+  const [formInput, setFormInput] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState<null | string>();
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(e);
+    const userInfo: RegisterPostRequestType = {
+      user: {
+        username: e.target.username.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      },
+    };
+    signOutUser(userInfo);
   };
+
+  const signOutUser = async (userInfo: RegisterPostRequestType) => {
+    await postUserRegister(userInfo).then((res) => {
+      if (res.errors) {
+        console.log(Object.keys(res.errors));
+        console.log(Object.values(res.errors));
+        const errorText = `${Object.keys(res.errors)} ${
+          Object.values(res.errors)[0]
+        }`;
+        setError(errorText);
+      } else {
+        router.push('/login');
+      }
+    });
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -17,13 +54,24 @@ const page = () => {
               <a href="/login">Have an account?</a>
             </p>
 
-            <ul className="error-messages">
-              <li>That email is already taken</li>
-            </ul>
+            {error && (
+              <ul className="error-messages">
+                <li>{error}</li>
+              </ul>
+            )}
 
             <form onSubmit={handleSubmit}>
               <fieldset className="form-group">
                 <input
+                  value={formInput.username}
+                  onChange={(e) =>
+                    setFormInput(
+                      produce((input) => {
+                        input.username = e.target.value;
+                      }),
+                    )
+                  }
+                  name="username"
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Username"
@@ -31,6 +79,15 @@ const page = () => {
               </fieldset>
               <fieldset className="form-group">
                 <input
+                  value={formInput.email}
+                  onChange={(e) =>
+                    setFormInput(
+                      produce((input) => {
+                        input.email = e.target.value;
+                      }),
+                    )
+                  }
+                  name="email"
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Email"
@@ -38,12 +95,28 @@ const page = () => {
               </fieldset>
               <fieldset className="form-group">
                 <input
+                  value={formInput.password}
+                  onChange={(e) =>
+                    setFormInput(
+                      produce((input) => {
+                        input.password = e.target.value;
+                      }),
+                    )
+                  }
+                  name="password"
                   className="form-control form-control-lg"
                   type="password"
                   placeholder="Password"
                 />
               </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right">
+              <button
+                disabled={
+                  formInput.email.length === 0 ||
+                  formInput.username.length === 0 ||
+                  formInput.password.length === 0
+                }
+                className="btn btn-lg btn-primary pull-xs-right"
+              >
                 Sign up
               </button>
             </form>
