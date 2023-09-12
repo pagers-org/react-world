e1076d2d50337d37cdbb0e406c1cd7456c2aa026
Jiwon Han <nomad.jiwhan@gmail.com>
2023-09-13 03:08:55 +0900

    chore: additonal configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,20 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../components/**/*.mdx', '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../components/**/*.stories.mdx','../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: {
+          implementation: require.resolve('postcss'),
+        },
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
