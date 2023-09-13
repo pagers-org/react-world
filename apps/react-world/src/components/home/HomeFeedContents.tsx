4a7b33d902c2a9d769a812799667587ecac21842
Wooseong Kim <innocarpe@gmail.com>
2023-09-13 23:04:58 +0900

    feat(home): 홈 화면에서 새로 작성한 ArticlePreview 를 사용하도록 변경

--- apps/react-world/src/components/home/HomeFeedContents.tsx
+++ apps/react-world/src/components/home/HomeFeedContents.tsx
@@ -11,26 +11,31 @@ const HomeFeedContents = () => (
         <HomeFeedTab activeFeed="global_feed" />
         {/* 더미 데이터 */}
         <ArticlePreview
-          authorProfileLink="/profile/eric-simons"
-          authorProfileImageUrl="http://i.imgur.com/Qr71crq.jpg"
-          authorName="Eric Simons"
-          publishDate="January 20th"
-          likeCount={29}
-          articleLink="/article/how-to-build-webapps-that-scale"
-          articleTitle="How to build webapps that scale"
-          articleDescription="This is the description for the post."
-          tags={['realworld', 'implementations']}
+          articlePreview={{
+            authorProfileLink: '/profile/eric-simons',
+            authorProfileImageUrl: 'http://i.imgur.com/Qr71crq.jpg',
+            authorName: 'Eric Simons',
+            publishDate: 'January 20th',
+            likeCount: 29,
+            articleLink: '/article/how-to-build-webapps-that-scale',
+            articleTitle: 'How to build webapps that scale',
+            articleDescription: 'This is the description for the post.',
+            tags: ['realworld', 'implementations'],
+          }}
         />
         <ArticlePreview
-          authorProfileLink="/profile/albert-pai"
-          authorProfileImageUrl="http://i.imgur.com/N4VcUeJ.jpg"
-          authorName="Albert Pai"
-          publishDate="January 20th"
-          likeCount={32}
-          articleLink="/article/the-song-you"
-          articleTitle="The song you won't ever stop singing. No matter how hard you try."
-          articleDescription="This is the description for the post."
-          tags={['realworld', 'implementations']}
+          articlePreview={{
+            authorProfileLink: '/profile/albert-pai',
+            authorProfileImageUrl: 'http://i.imgur.com/N4VcUeJ.jpg',
+            authorName: 'Albert Pai',
+            publishDate: 'January 20th',
+            likeCount: 32,
+            articleLink: '/article/the-song-you',
+            articleTitle:
+              "The song you won't ever stop singing. No matter how hard you try.",
+            articleDescription: 'This is the description for the post.',
+            tags: ['realworld', 'implementations'],
+          }}
         />
         <Pagination pages={[1, 2]} activePage={1} />
       </div>
