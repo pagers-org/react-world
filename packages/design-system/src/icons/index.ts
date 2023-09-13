a7d8ee2f15c56c3ba486c715e07b68e7f6d14ab1
seulgi <sgsg9447@gmail.com>
2023-09-13 17:43:14 +0900

    setting: icons 폴더 생성, react-icons설치

---
+++ packages/design-system/src/icons/index.ts
@@ -0,0 +1,5 @@
+export { AiFillHeart as HeartIcon } from "react-icons/ai";
+export { AiOutlinePlus as PlusIcon } from "react-icons/ai";
+export { IoIosSettings as SettingsIcon } from "react-icons/io";
+export { MdModeEditOutline as EditIcon } from "react-icons/md";
+export { RiDeleteBin6Fill as DeleteIcon } from "react-icons/ri";
