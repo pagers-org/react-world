85a0d167af33fd076d3e8a95f275b06f97c1d0e6
Wooseong Kim <innocarpe@gmail.com>
2023-09-14 00:17:57 +0900

    feat(home): ArticlePreviewService 작성

--- apps/react-world/src/apis/article/ArticlePreviewService.types.ts
+++ apps/react-world/src/apis/article/ArticlePreviewService.types.ts
@@ -1,11 +1,30 @@
+export interface ArticlePreviewParams {
+  offset: number;
+  limit: number;
+}
+
+// TODO: 클라이언트에서 좀 더 가독성 높은 선언이 되도록 프로퍼티 이름 변경하고 매핑 로직 작성할 것
+export interface ArticleAuthor {
+  username: string;
+  bio: string;
+  image: string;
+  following: boolean;
+}
+
+export interface ArticlePreviewData {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: ArticleAuthor;
+}
+
 export interface ArticlePreviewResponse {
-  authorProfileLink: string;
-  authorProfileImageUrl: string;
-  authorName: string;
-  publishDate: string;
-  likeCount: number;
-  articleLink: string;
-  articleTitle: string;
-  articleDescription: string;
-  tags: string[];
+  articles: ArticlePreviewData[];
+  articlesCount: number;
 }
