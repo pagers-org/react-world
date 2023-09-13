fcbacb6e291fb20619a9fba0d57e6b287abec58f
KyungJun <natcho9010@gmail.com>
2023-09-13 20:55:45 +0900

    chore: add openApi generator

--- packages/client/proto/generated/clientService/ClientRequest.ts
+++
@@ -1,9 +0,0 @@
-// Original file: proto/clientService.proto
-
-export interface ClientRequest {
-  id?: number;
-}
-
-export interface ClientRequest__Output {
-  id: number;
-}
--- packages/server/src/proto/generated/clientService/ClientRequest.ts
+++
@@ -1,9 +0,0 @@
-// Original file: src/proto/clientService.proto
-
-export interface ClientRequest {
-  id?: number;
-}
-
-export interface ClientRequest__Output {
-  id: number;
-}
