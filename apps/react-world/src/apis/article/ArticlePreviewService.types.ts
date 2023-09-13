b21201dd222d9bdc7da9fa88e4f47c34486184b5
Wooseong Kim <innocarpe@gmail.com>
2023-09-13 23:03:55 +0900

    feat(home): 홈 페이지를 렌더링할 때 props 로 전달받을 ArticlePreview 응답 선언

---
+++ apps/react-world/src/apis/article/ArticlePreviewService.types.ts
@@ -0,0 +1,11 @@
+export interface ArticlePreviewResponse {
+  authorProfileLink: string;
+  authorProfileImageUrl: string;
+  authorName: string;
+  publishDate: string;
+  likeCount: number;
+  articleLink: string;
+  articleTitle: string;
+  articleDescription: string;
+  tags: string[];
+}
