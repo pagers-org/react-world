f9d63ee976dfb711e6072ac80edf5a28d81c0416
Wooseong Kim <innocarpe@gmail.com>
2023-09-14 00:52:42 +0900

    feat(home): ArticlePreview 가 실제 응답 모델을 받아서 전처리 후 렌더링하도록 함

--- apps/react-world/src/components/home/ArticlePreview.tsx
+++ apps/react-world/src/components/home/ArticlePreview.tsx
@@ -1,4 +1,4 @@
-import type { ArticlePreviewResponse } from '../../apis/article/ArticlePreviewService.types';
+import type { ArticlePreviewData } from '../../apis/article/ArticlePreviewService.types';
 import {
   ArticlePreviewContainer,
   ArticlePreviewMeta,
@@ -17,44 +17,47 @@ import {
 } from './ArticlePreview.styled';
 
 interface ArticlePreviewProps {
-  articlePreview: ArticlePreviewResponse;
+  articlePreview: ArticlePreviewData;
 }
 
-const ArticlePreview = ({
-  articlePreview: {
-    authorProfileLink,
-    authorProfileImageUrl,
-    authorName,
-    publishDate,
-    likeCount,
-    articleLink,
-    articleTitle,
-    articleDescription,
-    tags,
-  },
-}: ArticlePreviewProps) => (
-  <ArticlePreviewContainer>
-    <ArticlePreviewMeta>
-      <AuthorProfileLink href={authorProfileLink}>
-        <AuthorProfileImage src={authorProfileImageUrl} />
-      </AuthorProfileLink>
-      <AuthorInfo>
-        <Author href={authorProfileLink}>{authorName}</Author>
-        <PublishedDate>{publishDate}</PublishedDate>
-      </AuthorInfo>
-      <LikeButton>{likeCount}</LikeButton>
-    </ArticlePreviewMeta>
-    <ArticlePreviewLink href={articleLink}>
-      <ArticleTitle>{articleTitle}</ArticleTitle>
-      <ArticleDescription>{articleDescription}</ArticleDescription>
-      <ReadMore>Read more...</ReadMore>
-      <TagList>
-        {tags.map(tag => (
-          <TagItem key={tag}>{tag}</TagItem>
-        ))}
-      </TagList>
-    </ArticlePreviewLink>
-  </ArticlePreviewContainer>
-);
+const ArticlePreview = ({ articlePreview }: ArticlePreviewProps) => {
+  const authorProfileLink = `/profile/${articlePreview.author.username}`;
+  const authorProfileImageUrl = articlePreview.author.image;
+  const authorName = articlePreview.author.username;
+
+  const publishDate = new Date(articlePreview.createdAt).toDateString();
+
+  const articleLink = `/article/${articlePreview.slug}`;
+  const articleTitle = articlePreview.title;
+  const articleDescription = articlePreview.description;
+
+  const likeCount = articlePreview.favoritesCount;
+  const tagsList = articlePreview.tagList;
+
+  return (
+    <ArticlePreviewContainer>
+      <ArticlePreviewMeta>
+        <AuthorProfileLink href={authorProfileLink}>
+          <AuthorProfileImage src={authorProfileImageUrl} />
+        </AuthorProfileLink>
+        <AuthorInfo>
+          <Author href={authorProfileLink}>{authorName}</Author>
+          <PublishedDate>{publishDate}</PublishedDate>
+        </AuthorInfo>
+        <LikeButton>{likeCount}</LikeButton>
+      </ArticlePreviewMeta>
+      <ArticlePreviewLink href={articleLink}>
+        <ArticleTitle>{articleTitle}</ArticleTitle>
+        <ArticleDescription>{articleDescription}</ArticleDescription>
+        <ReadMore>Read more...</ReadMore>
+        <TagList>
+          {tagsList.map(tag => (
+            <TagItem key={tag}>{tag}</TagItem>
+          ))}
+        </TagList>
+      </ArticlePreviewLink>
+    </ArticlePreviewContainer>
+  );
+};
 
 export default ArticlePreview;
