0ff3916ea7e4cecb5141d8c2468eeff066027bf1
71summernight <71summernight@naver.com>
2023-09-14 00:23:08 +0900

    안쓰는 react import 정리

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function DetailPage() {
   return <div>디테일페이지에 들어왔어요</div>;
 }
--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function UserArticlePage() {
   return <div>유저 아티클 페이지</div>;
 }
