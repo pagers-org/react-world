5a626428f8b493711bd2ed99e7c4f2aed5c694f5
ludacirs <ludacirs@naver.com>
2023-09-13 23:08:18 +0900

    fix: 좋아요 버튼을 토글로 변경

--- apps/realworld/src/widgets/article/index.ts
+++ apps/realworld/src/widgets/article/index.ts
@@ -1,4 +1,5 @@
+import ArticleFavoriteToggleButton from './article-favorite-toggle-button/article-favorite-toggle-button';
 import ArticleListItem from './article-list-item/article-list-item';
 import ArticleList from './article-list/article-list';
 
-export { ArticleListItem, ArticleList };
+export { ArticleListItem, ArticleList, ArticleFavoriteToggleButton };
