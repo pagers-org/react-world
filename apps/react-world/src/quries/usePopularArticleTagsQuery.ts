70b948dacc9f04ab28bd1799e1d19f2c1a73f248
Wooseong Kim <innocarpe@gmail.com>
2023-09-14 23:26:33 +0900

    feat(home): 인기 아티클 태그 목록을 조회하는 useQuery 작성

---
+++ apps/react-world/src/quries/usePopularArticleTagsQuery.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import PopularArticleTagService from '../apis/article/PopularArticleTagService';
+
+export const POPULAR_ARTICLE_TAG_CACHE_KEY = '@article/popular_tags';
+
+const usePopularArticleTagsQuery = () => {
+  PopularArticleTagService;
+  const queryResult = useQuery([POPULAR_ARTICLE_TAG_CACHE_KEY], () =>
+    PopularArticleTagService.fetchPopularArticleTags(),
+  );
+
+  return {
+    popularArticleTags: queryResult.data,
+    isPopularArticleTagsLoading: queryResult.isLoading,
+  };
+};
+
+export default usePopularArticleTagsQuery;
