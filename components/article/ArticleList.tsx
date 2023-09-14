dab9319fb3aaf5f89fed86747c98648d7060ca4b
hyeon9782 <hyeon9782@gmail.com>
2023-09-14 22:06:03 +0900

    feat: useInfiniteQuery test

--- components/article/ArticleList.tsx
+++ components/article/ArticleList.tsx
@@ -1,44 +1,33 @@
-// 'use client';
-import { ArticleAPI, fetchArticles } from '@/services/articles';
+'use client';
+import { ArticleAPI } from '@/services/articles';
 import ArticlePreview from './ArticlePreview';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import React, { useRef } from 'react';
+import React from 'react';
 
-const ArticleList = async () => {
-  const { articles } = await ArticleAPI.all(1);
+const ArticleList = () => {
+  const { data, error, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, status } = useInfiniteQuery({
+    queryKey: ['articles'],
+    queryFn: ({ pageParam = 1 }) => ArticleAPI.all(pageParam),
+    getNextPageParam: (lastPage, pages) => {
+      return true;
+    },
+    retry: false,
+  });
 
-  // const { data, error, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, status } = useInfiniteQuery({
-  //   queryKey: ['articles'],
-  //   queryFn: ({ pageParam = 1 }) => ArticleAPI.all(pageParam),
-  //   getNextPageParam: (lastPage, pages) => {
-  //     if (5 > page.current) {
-  //       return page.current;
-  //     }
-  //     return undefined;
-  //   },
-  //   retry: false,
-  //   onSuccess: () => {
-  //     console.log('성공' + page.current);
+  return (
+    <div>
+      <button onClick={() => fetchNextPage()}>다음</button>
+      {data?.pages.map((group, i) => (
+        <div key={i}>
+          {group.articles.map(article => (
+            <ArticlePreview key={article.slug} article={article} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 
-  //     page.current++;
-  //   },
-  //   onError: () => {},
-  // });
-
-  // return (
-  //   <div>
-  //     <button onClick={() => fetchNextPage()}>다음</button>
-  //     {data?.pages.map((group, i) => (
-  //       <div key={i}>
-  //         {group.articles.map(article => (
-  //           <ArticlePreview key={article.slug} article={article} />
-  //         ))}
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
-
-  return <div>{articles?.map(article => <ArticlePreview key={article.slug} article={article} />)}</div>;
+  // return <div>{articles?.map(article => <ArticlePreview key={article.slug} article={article} />)}</div>;
 };
 
 export default ArticleList;
