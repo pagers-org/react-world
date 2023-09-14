d7db31e33d2eb60f620f217e611621092e522cac
Wooseong Kim <innocarpe@gmail.com>
2023-09-14 21:53:44 +0900

    feat(home): Pagination 에서 페이지 클릭 이벤트를 상위로 전달하도록 onPageChange props 추가

--- apps/react-world/src/components/home/Pagination.tsx
+++ apps/react-world/src/components/home/Pagination.tsx
@@ -3,15 +3,31 @@ import { PaginationContainer, PageButton } from './Pagination.styled';
 interface PaginationProps {
   totalPages: number;
   activePageIndex: number;
+  onPageChange: (newPageIndex: number) => void;
 }
 
-const Pagination = ({ totalPages, activePageIndex }: PaginationProps) => {
+const Pagination = ({
+  totalPages,
+  activePageIndex,
+  onPageChange,
+}: PaginationProps) => {
   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  const handlePageClick = (pageIndex: number) => {
+    if (pageIndex !== activePageIndex) {
+      onPageChange(pageIndex);
+    }
+  };
 
   return (
     <PaginationContainer>
       {pages.map((page, index) => (
-        <PageButton key={page} href="#" isActive={index === activePageIndex}>
+        <PageButton
+          key={page}
+          href="#"
+          isActive={index === activePageIndex}
+          onClick={() => handlePageClick(index)}
+        >
           {page}
         </PageButton>
       ))}
