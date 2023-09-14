fe9351a9ea582160a9253d4b2a7e4e331fc6427d
henry <70426440+hyjoong@users.noreply.github.com>
2023-09-15 00:09:01 +0900

    feat: add temporary variables to constants

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const TEMP_POSTS = [
+  { title: 'title1', content: 'content1' },
+  { title: 'title2', content: 'content2' },
+  { title: 'title3', content: 'content3' },
+];
+
+export const TEMP_CONTENT =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit Exercitationem sint soluta laudantium porro inventore sit debitis voluptates, nam perferendis cupiditate dolor minus doloribus quia voluptatibus ab voluptas? Saepe, magnam numquam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sint soluta laudantium porro inventore sit debitis voluptates, nam perferendis cupiditate dolor minus doloribus quia voluptatibus ab voluptas? Saepe, magnam numquam.';
