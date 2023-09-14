88f8f1629d94eae3c0b8f6e51e76681ba5a22c5b
Wooseong Kim <innocarpe@gmail.com>
2023-09-15 01:57:13 +0900

    chore(app):  ReactQueryDevtools 셋업
    (cherry picked from commit 46c6144ea57d9390de18fbe42d92c42886d6bbeb)

--- apps/react-world/src/main.tsx
+++ apps/react-world/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Routes } from './routes';
 import '../styles/main.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Routes />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
