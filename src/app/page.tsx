e88cfbc4c8a71df19c0f50322752c639d162579e
henry <70426440+hyjoong@users.noreply.github.com>
2023-09-15 00:14:34 +0900

    feat: root page imported postCardList, TagList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { PostCardList } from '@/components/home/PostCardList';
+import { TagList } from '@/components/home/TagList';
 
 const RootPage = () => {
-  return <div>RootPage</div>;
+  const TEMP_TAGS = ['programming', 'react', 'javascript', 'typescript'];
+  return (
+    <div>
+      <div className="flex">
+        <PostCardList />
+        <TagList tags={TEMP_TAGS} />
+      </div>
+    </div>
+  );
 };
 
 export default RootPage;
