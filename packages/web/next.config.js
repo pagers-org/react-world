292398b3ceef884fd2c2c4af51eebb428d236ac7
Sangmin Park <sangminpark@Sangminui-MacBookPro-2.local>
2023-09-15 10:42:13 +0900

    chore: Prettier와 vanilla-extract 기본 설정을 추가합니다.

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const createVanillaExtractPlugin =
+  require('@vanilla-extract/next-plugin').createVanillaExtractPlugin
+const withVanillaExtract = createVanillaExtractPlugin({
+  identifiers: ({ hash }) => `prefix_${hash}`,
+})
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+module.exports = withVanillaExtract(nextConfig)
