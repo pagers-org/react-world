f381cfaa001493ce9318e4b3f81a9b7664271467
ding-co <pangoons@naver.com>
2023-09-17 14:44:28 +0900

    feat: Add profile API fetcher fn, types

---
+++ src/api/profile.ts
@@ -0,0 +1,64 @@
+import { ProfileResponse } from '@/types/api/profile';
+
+import { COMMON_HEADERS, HTTP_METHOD } from '@/constants/api';
+import { API_BASE_URL } from '@/constants/env';
+
+/* Client Side APIs */
+
+// Follow a user
+export const postFollowUser = (
+  username: string,
+  headers: HeadersInit = {},
+  options: RequestInit = {},
+): Promise<ProfileResponse> => {
+  return fetch(`${API_BASE_URL}/profiles/${username}/follow`, {
+    ...options,
+    method: HTTP_METHOD.POST,
+    headers: {
+      ...COMMON_HEADERS,
+      ...headers,
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
+
+// Unfollow a user
+export const deleteUnfollowUser = (
+  username: string,
+  headers: HeadersInit = {},
+  options: RequestInit = {},
+): Promise<ProfileResponse> => {
+  return fetch(`${API_BASE_URL}/profiles/${username}/follow`, {
+    ...options,
+    method: HTTP_METHOD.DELETE,
+    headers: {
+      ...COMMON_HEADERS,
+      ...headers,
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
+
+// -----------------------------------------------------------------------------------------------------
+
+/* Server Side APIs for Next 13 APP Router extended fetch api */
+
+// Get a Profile (Auth Optional)
+export const getProfile = (
+  username: string,
+  headers: HeadersInit = {},
+  options: RequestInit = {},
+): Promise<ProfileResponse> => {
+  return fetch(`${API_BASE_URL}/profiles/${username}`, {
+    ...options,
+    method: HTTP_METHOD.GET,
+    headers: {
+      ...COMMON_HEADERS,
+      ...headers,
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
---
+++ src/types/api/profile.d.ts
@@ -0,0 +1,8 @@
+export type ProfileResponse = {
+  profile: {
+    username: string;
+    bio: string;
+    image: string;
+    following: boolean;
+  };
+};
