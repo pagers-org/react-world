b119590121faefc07138fb79d00bfe30cee4f8ab
Wooseong Kim <innocarpe@gmail.com>
2023-09-17 23:54:59 +0900

    feat(app): Routes 를 react-router-dom v6 스타일로 리팩토링하고 AppRoutes 로 리네이밍
    - 아티클 상세화면 쪽 slug 를 articleSlug 로 리네이밍했습니다.

--- apps/react-world/src/main.tsx
+++ apps/react-world/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes } from './routes';
+import { AppRoutes } from './routes';
 import '../styles/main.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Routes />
+      <AppRoutes />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
---
+++ apps/react-world/src/routes/AppRoutes.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from '../components/shared/Layout/Layout';
+import { HomePage } from '../pages/home';
+import { LoginPage } from '../pages/login';
+import { RegisterPage } from '../pages/register';
+import { ProfilePage } from '../pages/profile';
+import { ArticlePage } from '../pages/article';
+import { EditorPage } from '../pages/editor';
+import { SettingsPage } from '../pages/settings';
+import { Global } from '@emotion/react';
+import globalStyles from '../../styles/globalStyles';
+
+export const AppRoutes = () => {
+  return (
+    <Layout>
+      <Global styles={globalStyles} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route path="/editor/:articleSlug?" element={<EditorPage />} />
+          <Route path="/article/:articleSlug" element={<ArticlePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </Router>
+    </Layout>
+  );
+};
