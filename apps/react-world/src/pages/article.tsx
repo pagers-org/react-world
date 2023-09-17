6f034c3b8bb52e8d5fb0e8fe7100a04169a2bf06
Wooseong Kim <innocarpe@gmail.com>
2023-09-17 18:28:31 +0900

    fix(article): 중복이던 상단 네비바와 하단 푸터 영역 제거

--- apps/react-world/src/pages/article.tsx
+++ apps/react-world/src/pages/article.tsx
@@ -7,31 +7,6 @@ export const ArticlePage = () => {
 
   return (
     <>
-      <nav className="navbar navbar-light">
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            conduit
-          </a>
-          <ul className="nav navbar-nav pull-xs-right">
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/login">
-                Sign in
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/register">
-                Sign up
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
       <div className="article-page">
         <div className="banner">
           <div className="container">
@@ -188,19 +163,6 @@ export const ArticlePage = () => {
           </div>
         </div>
       </div>
-
-      <footer>
-        <div className="container">
-          <a href="/" className="logo-font">
-            conduit
-          </a>
-          <span className="attribution">
-            An interactive learning project from{' '}
-            <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
-            licensed under MIT.
-          </span>
-        </div>
-      </footer>
     </>
   );
 };
