8dcb0c12bc6b6445e3835ada368a7b04144a2367
cham0287 <crtmt97@gmail.com>
2023-09-18 22:17:46 +0900

    Feat: 비로그인시 빈authorization 로직 추가

--- src/services/app.ts
+++ src/services/app.ts
@@ -15,7 +15,7 @@ export const executeAPI = async ({ method, body, url }: ExecuteAPIProps) => {
     method,
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
-      Authorization: authorization,
+      Authorization: token ? authorization : "",
     },
     body: JSON.stringify(body),
   });
