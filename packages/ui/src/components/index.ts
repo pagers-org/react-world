950e78d96bba1ffd8c823ff8bec59c750afcd5df
유제호 <45571631+ludacirs@users.noreply.github.com>
2023-09-18 11:51:26 +0900

    Input 공용 컴포넌트를 추가합니다. (#126)
    * [ui] feat: Input 컴포넌트 추가
    
    * [ui] fix: `Button` 컴포넌트 disabled, fullWidth 스타일 추가
    
    * [ui] fix: `Input` 컴포넌트 fullWidth 스타일 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,6 +1,7 @@
 import Avatar from "./Avatar/Avatar";
 import Button from "./Button/Button";
+import Input from "./Input/Input";
 import Tabs from "./Tabs/Tabs";
 import Tag from "./Tag/Tag";
 
-export { Button, Tag, Tabs, Avatar };
+export { Button, Tag, Tabs, Avatar, Input };
