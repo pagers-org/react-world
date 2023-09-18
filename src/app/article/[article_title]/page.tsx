9a75dbe98cfcda3713dc6a730853e2995eb217a7
ding-co <pangoons@naver.com>
2023-09-18 18:03:11 +0900

    update: article detail page [article_title] -> [slug]

--- src/app/article/[article_title]/page.tsx
+++
@@ -1,7 +0,0 @@
-import ArticleDetailPageMain from '@/pageComponents/article/[article_title]/ArticleDetailPageMain';
-
-const ArticleDetailPage = () => {
-  return <ArticleDetailPageMain />;
-};
-
-export default ArticleDetailPage;
---
+++ src/app/article/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+import ArticleDetailPageMain from '@/pageComponents/article/[article_title]/ArticleDetailPageMain';
+
+import { COOKIE_ACCESS_TOKEN_KEY } from '@/constants/key';
+
+import { getArticle } from '@/api/articles';
+
+const getArticleDetail = async (slug: string) => {
+  const cookieStore = cookies();
+
+  const accessToken = cookieStore.get(COOKIE_ACCESS_TOKEN_KEY)?.value;
+
+  if (!slug) {
+    redirect('/');
+  }
+
+  const res = await getArticle(slug, {
+    Authorization: `Bearer ${accessToken}`,
+  });
+
+  if (res?.errors) {
+    redirect('/');
+  }
+
+  return res.article;
+};
+
+const ArticleDetailPage = async ({ params }: { params: { slug: string } }) => {
+  const article = await getArticleDetail(params.slug);
+
+  return <ArticleDetailPageMain article={article} />;
+};
+
+export default ArticleDetailPage;
