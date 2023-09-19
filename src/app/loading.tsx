14fcc5b9a80b002240cecac357c6224e9aad08a7
ding-co <pangoons@naver.com>
2023-09-19 21:18:08 +0900

    feat: Add HomePage loading (root)

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+const Loading = () => {
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1 className="logo-font">conduit</h1>
+          <p>A place to share your knowledge.</p>
+        </div>
+      </div>
+
+      <div className="container page">
+        <div className="row">Loading...</div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
