c8d394db1c8758fdb6fb2110af49703be364a68a
ding-co <pangoons@naver.com>
2023-09-19 22:24:29 +0900

    feat: Add useArticlesQuery hook

---
+++ src/hooks/query/articles/useArticlesQuery.ts
@@ -0,0 +1,20 @@
+import { queryKeys } from '@/react-query/queryKeys';
+import { FeedQueryStrings } from '@/types/api/articles';
+import { useQuery } from '@tanstack/react-query';
+
+import { getGlobalFeeds } from '@/api/articles';
+
+export const useCurationQuery = (
+  queryStrings: FeedQueryStrings = {
+    limit: '20',
+  },
+  headers: HeadersInit = {},
+  options: RequestInit = {},
+) => {
+  const { data, isLoading, isSuccess, isError, isFetching, isFetched, error } =
+    useQuery([queryKeys.GetArticles, queryStrings], () =>
+      getGlobalFeeds(queryStrings, headers, options),
+    );
+
+  return { data, isLoading, isSuccess, isError, isFetching, isFetched, error };
+};
