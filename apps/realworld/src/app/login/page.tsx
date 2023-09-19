019b02a5f8867e0e29b3af7d6a1d3863f797cb6e
유제호 <45571631+ludacirs@users.noreply.github.com>
2023-09-20 00:04:16 +0900

    로그인/회원가입 페이지를 만듭니다. (#133)
    * feat: `login` 페이지 구현
    
    * feat: `register` 페이지 구현
    
    * fix: gnb active 스타일 수정

--- apps/realworld/src/app/login/page.tsx
+++ apps/realworld/src/app/login/page.tsx
@@ -1,36 +1,21 @@
+import { LoginForm } from '@/features/user';
 import { PathBuilder } from '@/shared/utils/routes';
 import React from 'react';
 
 const Login = () => {
   return (
-    <React.Fragment>
-      <div className="auth-page">
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign in</h1>
-              <p className="text-xs-center">
-                <a href={PathBuilder.buildRegister().getPath()}>Need an account?</a>
-              </p>
-
-              <ul className="error-messages">
-                <li>That email is already taken</li>
-              </ul>
-
-              <form>
-                <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Email" />
-                </fieldset>
-                <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="password" placeholder="Password" />
-                </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
-              </form>
-            </div>
-          </div>
+    <div className="flex flex-col items-center w-full">
+      <div className="flex flex-col gap-16 w-540">
+        <div className="flex flex-col gap-8">
+          <h1 className="text-[2.5rem] text-center">Sign in</h1>
+          <p className="text-center text-green600">
+            <a href={PathBuilder.buildRegister().getPath()}>Need an account?</a>
+          </p>
         </div>
+
+        <LoginForm />
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
--- apps/realworld/src/features/user/ui/index.ts
+++ apps/realworld/src/features/user/ui/index.ts
@@ -1,3 +1,5 @@
+import LoginForm from './login-form/login-form';
+import RegisterForm from './register-form/register-form';
 import UserFollowToggleButton from './user-follow-toggle-button/user-follow-toggle-button';
 
-export { UserFollowToggleButton };
+export { UserFollowToggleButton, LoginForm, RegisterForm };
