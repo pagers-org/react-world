916065ea56c7962750630532628e945194b25d1d
ludacirs <ludacirs@naver.com>
2023-09-19 22:50:50 +0900

    feat: react-query devtools 설치 및 적용

--- apps/realworld/src/providers/Providers.tsx
+++ apps/realworld/src/providers/Providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React, { ReactNode, useState } from 'react';
 
 interface ProviderProps {
@@ -18,7 +19,12 @@ const Provider = ({ children }: ProviderProps) => {
         },
       }),
   );
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default Provider;
