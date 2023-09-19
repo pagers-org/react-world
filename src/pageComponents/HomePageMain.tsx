142db364135fd84ac916b37ae0112f02e558abbf
ding-co <pangoons@naver.com>
2023-09-19 21:17:49 +0900

    update: HomePageMain tabsElement conditional jsx

--- src/pageComponents/HomePageMain.tsx
+++ src/pageComponents/HomePageMain.tsx
@@ -21,7 +21,7 @@ const HomePageMain = ({ feeds }: Props) => {
   const [feedType, setFeedType] = useState<'your' | 'global'>();
   const [page, setPage] = useState<number>(1);
 
-  const [tabsElement, setTabsElement] = useState<ReactNode>(<></>);
+  const [tabsElement, setTabsElement] = useState<ReactNode>();
 
   const { articlesCount, articles } = feeds;
 
@@ -87,112 +87,118 @@ const HomePageMain = ({ feeds }: Props) => {
   }, [user.email, feedType]);
 
   return (
-    <div className="home-page">
-      <div className="banner">
-        <div className="container">
-          <h1 className="logo-font">conduit</h1>
-          <p>A place to share your knowledge.</p>
+    tabsElement && (
+      <div className="home-page">
+        <div className="banner">
+          <div className="container">
+            <h1 className="logo-font">conduit</h1>
+            <p>A place to share your knowledge.</p>
+          </div>
         </div>
-      </div>
 
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-9">
-            <div className="feed-toggle">{tabsElement}</div>
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-9">
+              <div className="feed-toggle">{tabsElement}</div>
 
-            {articles.map(
-              ({
-                slug,
-                title,
-                description,
-                tagList,
-                createdAt,
-                favoritesCount,
-                author: { username: authorUsername, image: authorImage },
-              }) => (
-                <div key={slug} className="article-preview">
-                  <div className="article-meta">
-                    <Link href={`/profile/${authorUsername}`}>
-                      <Image
-                        src={authorImage}
-                        alt={authorUsername}
-                        width={32}
-                        height={32}
-                      />
-                    </Link>
-                    <div className="info">
-                      <Link
-                        href={`/profile/${authorUsername}`}
-                        className="author"
-                      >
-                        {authorUsername}
+              {articles.map(
+                ({
+                  slug,
+                  title,
+                  description,
+                  tagList,
+                  createdAt,
+                  favoritesCount,
+                  author: { username: authorUsername, image: authorImage },
+                }) => (
+                  <div key={slug} className="article-preview">
+                    <div className="article-meta">
+                      <Link href={`/profile/${authorUsername}`}>
+                        <Image
+                          src={authorImage}
+                          alt={authorUsername}
+                          width={32}
+                          height={32}
+                        />
                       </Link>
-                      <span className="date">{createdAt}</span>
-                    </div>
-                    <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                      <i className="ion-heart"></i> {favoritesCount}
-                    </button>
-                  </div>
-                  <Link
-                    href="/article/how-to-build-webapps-that-scale"
-                    className="preview-link"
-                  >
-                    <h1>{title}</h1>
-                    <p>{description}</p>
-                    <span>Read more...</span>
-                    <ul className="tag-list">
-                      {tagList.map((tag) => (
-                        <li
-                          key={tag}
-                          className="tag-default tag-pill tag-outline"
+                      <div className="info">
+                        <Link
+                          href={`/profile/${authorUsername}`}
+                          className="author"
                         >
-                          {tag}
-                        </li>
-                      ))}
-                    </ul>
+                          {authorUsername}
+                        </Link>
+                        <span className="date">{createdAt}</span>
+                      </div>
+                      <button className="btn btn-outline-primary btn-sm pull-xs-right">
+                        <i className="ion-heart"></i> {favoritesCount}
+                      </button>
+                    </div>
+                    <Link
+                      href="/article/how-to-build-webapps-that-scale"
+                      className="preview-link"
+                    >
+                      <h1>{title}</h1>
+                      <p>{description}</p>
+                      <span>Read more...</span>
+                      <ul className="tag-list">
+                        {tagList.map((tag) => (
+                          <li
+                            key={tag}
+                            className="tag-default tag-pill tag-outline"
+                          >
+                            {tag}
+                          </li>
+                        ))}
+                      </ul>
+                    </Link>
+                  </div>
+                ),
+              )}
+
+              <Pagination
+                totalCounts={articlesCount}
+                page={page}
+                perPage={10}
+              />
+            </div>
+
+            <div className="col-md-3">
+              <div className="sidebar">
+                <p>Popular Tags</p>
+
+                <div className="tag-list">
+                  <Link href="" className="tag-pill tag-default">
+                    programming
+                  </Link>
+                  <Link href="" className="tag-pill tag-default">
+                    javascript
+                  </Link>
+                  <Link href="" className="tag-pill tag-default">
+                    emberjs
+                  </Link>
+                  <Link href="" className="tag-pill tag-default">
+                    angularjs
+                  </Link>
+                  <Link href="" className="tag-pill tag-default">
+                    react
+                  </Link>
+                  <Link href="" className="tag-pill tag-default">
+                    mean
+                  </Link>
+                  <Link href="" className="tag-pill tag-default">
+                    node
+                  </Link>
+                  <Link href="" className="tag-pill tag-default">
+                    rails
                   </Link>
                 </div>
-              ),
-            )}
-
-            <Pagination totalCounts={articlesCount} page={page} perPage={10} />
-          </div>
-
-          <div className="col-md-3">
-            <div className="sidebar">
-              <p>Popular Tags</p>
-
-              <div className="tag-list">
-                <Link href="" className="tag-pill tag-default">
-                  programming
-                </Link>
-                <Link href="" className="tag-pill tag-default">
-                  javascript
-                </Link>
-                <Link href="" className="tag-pill tag-default">
-                  emberjs
-                </Link>
-                <Link href="" className="tag-pill tag-default">
-                  angularjs
-                </Link>
-                <Link href="" className="tag-pill tag-default">
-                  react
-                </Link>
-                <Link href="" className="tag-pill tag-default">
-                  mean
-                </Link>
-                <Link href="" className="tag-pill tag-default">
-                  node
-                </Link>
-                <Link href="" className="tag-pill tag-default">
-                  rails
-                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
