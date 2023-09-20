7c34a006fcc3b23ca4e125ae1fd72060fed91b7c
Wooseong Kim <innocarpe@gmail.com>
2023-09-20 23:19:02 +0900

    feat(article): ArticleComments 컴포넌트에서 아티클 코멘트 목록을 주입받아 렌더링하도록 변경

--- apps/react-world/src/components/article/ArticleComments.tsx
+++ apps/react-world/src/components/article/ArticleComments.tsx
@@ -1,4 +1,12 @@
-const ArticleComments = () => {
+import type { ArticleCommentData } from '@apis/article/ArticleService.types';
+
+interface ArticleCommentsProps {
+  comments: ArticleCommentData[];
+}
+
+const ArticleComments = (props: ArticleCommentsProps) => {
+  const { comments } = props;
+
   return (
     <div className="row">
       <div className="col-xs-12 col-md-8 offset-md-2">
@@ -10,6 +18,7 @@ const ArticleComments = () => {
             ></textarea>
           </div>
           <div className="card-footer">
+            {/* This will likely be the logged-in user's image */}
             <img
               src="http://i.imgur.com/Qr71crq.jpg"
               className="comment-author-img"
@@ -18,52 +27,39 @@ const ArticleComments = () => {
           </div>
         </form>
 
-        <div className="card">
-          <div className="card-block">
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
+        {comments.map(comment => (
+          <div key={comment.id} className="card">
+            <div className="card-block">
+              <p className="card-text">{comment.body}</p>
+            </div>
+            <div className="card-footer">
+              <a
+                href={`/profile/${comment.author.username}`}
+                className="comment-author"
+              >
+                <img
+                  src={comment.author.image}
+                  className="comment-author-img"
+                />
+              </a>
+              &nbsp;
+              <a
+                href={`/profile/${comment.author.username}`}
+                className="comment-author"
+              >
+                {comment.author.username}
+              </a>
+              <span className="date-posted">
+                {new Date(comment.createdAt).toDateString()}
+              </span>
+              {/* If the logged-in user has the right to delete this comment, display the delete button */}
+              {/* For now, I'm leaving it static but you'll likely need a condition */}
+              <span className="mod-options">
+                <i className="ion-trash-a"></i>
+              </span>
+            </div>
           </div>
-          <div className="card-footer">
-            <a href="/profile/author" className="comment-author">
-              <img
-                src="http://i.imgur.com/Qr71crq.jpg"
-                className="comment-author-img"
-              />
-            </a>
-            &nbsp;
-            <a href="/profile/jacob-schmidt" className="comment-author">
-              Jacob Schmidt
-            </a>
-            <span className="date-posted">Dec 29th</span>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="card-block">
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-          </div>
-          <div className="card-footer">
-            <a href="/profile/author" className="comment-author">
-              <img
-                src="http://i.imgur.com/Qr71crq.jpg"
-                className="comment-author-img"
-              />
-            </a>
-            &nbsp;
-            <a href="/profile/jacob-schmidt" className="comment-author">
-              Jacob Schmidt
-            </a>
-            <span className="date-posted">Dec 29th</span>
-            <span className="mod-options">
-              <i className="ion-trash-a"></i>
-            </span>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
