6b58141507c41a7cc0388723eab5f65c1592e010
heo min <min@weffwd.com>
2023-09-20 21:09:33 +0900

    feat : header 에서 가져오는 params 를 page props 에서 가져올 수 있도록 수정합니다.

--- apps/web/app/(articles)/article/[slug]/page.tsx
+++ apps/web/app/(articles)/article/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import { headers } from "next/headers";
 import { dehydrate } from "@tanstack/query-core";
 import { Suspense } from "react";
 import getQueryClient from "../../../../lib/get-query-clinet";
@@ -7,17 +6,20 @@ import { getArticle } from "../../_api/get-article";
 import Hydrate from "../../../../lib/query-hydrate";
 import { ArticleDetail } from "../../_components/article-detail";
 
-export default async function ArticlePage(): Promise<JSX.Element> {
-  const headersList = headers();
-  const activePath = headersList.get("x-invoke-path");
-  const slug = activePath?.split("/")[2];
+interface ArticlePageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function ArticlePage({ params }: ArticlePageProps): Promise<JSX.Element> {
+  const { slug } = params;
 
   if (!slug) {
     throw new Error("ERROR : article path is not found");
   }
 
   const queryClient = getQueryClient();
-
   await queryClient.prefetchQuery(queryKeys.article(slug), () => getArticle(slug));
   const dehydrateState = dehydrate(queryClient);
 
