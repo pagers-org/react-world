93d2bc08a8e51bbba27f86eed3d9a4cfe91e5a74
Wooseong Kim <innocarpe@gmail.com>
2023-09-20 23:08:31 +0900

    chore(app): vite.config.ts 에 실수로 넣은 path 관련 import 제거

--- apps/react-world/vite.config.ts
+++ apps/react-world/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
