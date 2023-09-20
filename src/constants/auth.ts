9e07a9bcb095973d56672140dd4ffb6095cd69bc
moseung <tmdah900@naver.com>
2023-09-20 21:26:19 +0900

    fix: header 오타 수정

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,7 +1,7 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_END_POINT;
 
 const BASE_HEADER = {
-  'Content-Type': 'application/json ;charset: utf-8',
+  'Content-Type': 'application/json; charset=utf-8',
 };
 
 export { BASE_URL, BASE_HEADER };
