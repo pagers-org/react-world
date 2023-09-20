02571002dd546e62bfda2ae4a4767f515d557d8f
허민 <huhmin0409@naver.com>
2023-09-20 21:36:36 +0900

    feat : 임시로 로그인 상태를 구분합니다 (#136)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,19 @@
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const nextConfig = { reactStrictMode: true, transpilePackages: ["ui"] };
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["ui"],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/1",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = {};
 
