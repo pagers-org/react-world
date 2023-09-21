4ddb319dd9d6ffff03738d269a00bb7a03808047
Wooseong Kim <innocarpe@gmail.com>
2023-09-21 21:34:15 +0900

    feat(home): 작성한 useQuery 를 사용해 기본 쿼리 파라미터가 포함되지 않을 때 포함해 replace 를 먼저 하도록 처리

--- apps/react-world/src/pages/home.tsx
+++ apps/react-world/src/pages/home.tsx
@@ -1,11 +1,18 @@
-import HomePageContainer from '../components/home/HomePageContainer';
-import { HomeBanner } from '../components/home/HomeBanner';
-import HomeFeedContents from '../components/home/HomeFeedContents';
+import HomePageContainer from '@components/home/HomePageContainer';
+import { HomeBanner } from '@components/home/HomeBanner';
+import HomeFeedContents from '@components/home/HomeFeedContents';
 import { useLocation } from 'react-router-dom';
+import { useQueryParamSetter } from '@hooks/useQueryParamSetter';
 
 export const HomePage = () => {
   const location = useLocation();
-  const params = new URLSearchParams(location.search);
+  const currentParams = new URLSearchParams(location.search);
+
+  // 기본 쿼리 파라미터가 없으면 포함해서 다시 URL 로딩
+  const params = useQueryParamSetter(currentParams, {
+    feed: 'global',
+    page: '1',
+  });
 
   const feedType = params.get('feed') || 'global';
   const page = Number(params.get('page')) || 1;
