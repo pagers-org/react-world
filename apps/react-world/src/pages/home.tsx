486ad861f437c0e54aac9fc3245d967040ac1e08
Wooseong Kim <innocarpe@gmail.com>
2023-09-21 20:30:21 +0900

    feat(home): 홈 페이지에서 피드 타입과 페이지 쿼리 파라미터를 받아 HomeFeedContents 로 전달하도록 함

--- apps/react-world/src/pages/home.tsx
+++ apps/react-world/src/pages/home.tsx
@@ -1,12 +1,19 @@
 import HomePageContainer from '../components/home/HomePageContainer';
 import { HomeBanner } from '../components/home/HomeBanner';
 import HomeFeedContents from '../components/home/HomeFeedContents';
+import { useLocation } from 'react-router-dom';
 
 export const HomePage = () => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+
+  const feedType = params.get('feed') || 'global';
+  const page = Number(params.get('page')) || 1;
+
   return (
     <HomePageContainer>
       <HomeBanner />
-      <HomeFeedContents />
+      <HomeFeedContents feedType={feedType} page={page} />
     </HomePageContainer>
   );
 };
