cf4126ee3b5522f8fd227e67bdcfc11aa2d6354e
Wooseong Kim <innocarpe@gmail.com>
2023-09-21 23:06:27 +0900

    feat(home): 홈 페이지에서 작성한 Hook 기반으로 기본 파라미터 지정 처리를 하도록 함

--- apps/react-world/src/pages/home.tsx
+++ apps/react-world/src/pages/home.tsx
@@ -2,20 +2,23 @@ import HomePageContainer from '@components/home/HomePageContainer';
 import { HomeBanner } from '@components/home/HomeBanner';
 import HomeFeedContents from '@components/home/HomeFeedContents';
 import { useLocation } from 'react-router-dom';
-import { useQueryParamSetter } from '@hooks/useQueryParamSetter';
+import { useDefaultQueryParamsSetter } from '@hooks/useDefaultQueryParamsSetter';
+import { useConditionalReplace } from '@hooks/useConditionalReplace';
 
 export const HomePage = () => {
   const location = useLocation();
   const currentParams = new URLSearchParams(location.search);
 
   // 기본 쿼리 파라미터가 없으면 포함해서 다시 URL 로딩
-  const params = useQueryParamSetter(currentParams, {
-    feed: 'global',
-    page: '1',
-  });
+  const { hasParamsChanged: shouldRepace, newParams } =
+    useDefaultQueryParamsSetter(currentParams, {
+      feed: 'global',
+      page: '1',
+    });
+  useConditionalReplace(shouldRepace, `?${newParams.toString()}`);
 
-  const feedType = params.get('feed') || 'global';
-  const page = Number(params.get('page')) || 1;
+  const feedType = newParams.get('feed') || 'global';
+  const page = Number(newParams.get('page')) || 1;
 
   return (
     <HomePageContainer>
