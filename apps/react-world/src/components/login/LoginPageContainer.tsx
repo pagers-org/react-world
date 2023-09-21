5f3f47130ad8e61dd11d54bdf2b04c3664865633
Wooseong Kim <innocarpe@gmail.com>
2023-09-22 00:09:09 +0900

    feat(login): 로그인 페이지 컨테이너 내에서 분리한 각 컴포넌트를 사용하도록 함

--- apps/react-world/src/components/login/LoginPageContainer.tsx
+++ apps/react-world/src/components/login/LoginPageContainer.tsx
@@ -1,37 +1,22 @@
+import LoginErrorMessage from './LoginErrorMessageList';
+import LoginForm from './LoginForm';
+import LoginHeader from './LoginHeader';
+
 const LoginPageContainer = () => {
+  const handleLogin = (data: { email: string; password: string }) => {
+    console.log(data);
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign in</h1>
-            <p className="text-xs-center">
-              <a href="/register">Need an account?</a>
-            </p>
-
-            <ul className="error-messages">
-              <li>That email is already taken</li>
-            </ul>
-
-            <form>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="Email"
-                />
-              </fieldset>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="password"
-                  placeholder="Password"
-                />
-              </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right">
-                Sign in
-              </button>
-            </form>
+            <LoginHeader />
+            <LoginErrorMessage
+              errorMessages={['That email is already taken']}
+            />
+            <LoginForm onLogin={handleLogin} />
           </div>
         </div>
       </div>
