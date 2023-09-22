a597dca254c345b3341e8843a63eb455a5ba2cf8
Wooseong Kim <innocarpe@gmail.com>
2023-09-22 13:20:15 +0900

    feat(login): 로그인 페이지 단에서 에러 메시지를 리스트로 표시하는 기능 제거

--- apps/react-world/src/components/login/LoginPageContainer.tsx
+++ apps/react-world/src/components/login/LoginPageContainer.tsx
@@ -1,31 +1,9 @@
-import { useState } from 'react';
-import LoginErrorMessage from './LoginErrorMessageList';
 import LoginForm from './LoginForm';
 import LoginHeader from './LoginHeader';
-import type { FieldErrors } from 'react-hook-form';
 
 const LoginPageContainer = () => {
-  const [errorMessages, setErrorMessages] = useState<string[]>([]);
-
   const handleLogin = (data: { email: string; password: string }) => {
-    console.log(data);
-  };
-
-  const handleErrors = (errors: FieldErrors) => {
-    const newErrors: string[] = [];
-
-    if (
-      errors.email &&
-      typeof errors.email.message === 'string' &&
-      !errorMessages.includes(errors.email.message)
-    ) {
-      newErrors.push(errors.email.message);
-    }
-
-    const allErrors = [...errorMessages, ...newErrors];
-    const uniqueErrors = Array.from(new Set(allErrors));
-
-    setErrorMessages(uniqueErrors);
+    console.log('handleLogin: ' + JSON.stringify(data, null, 2));
   };
 
   return (
@@ -34,8 +12,7 @@ const LoginPageContainer = () => {
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
             <LoginHeader />
-            <LoginErrorMessage errorMessages={errorMessages} />
-            <LoginForm onLogin={handleLogin} onError={handleErrors} />
+            <LoginForm onLoginSubmit={handleLogin} />
           </div>
         </div>
       </div>
