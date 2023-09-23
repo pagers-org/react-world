fdac338a11ae74ad0b512abf6852dbb09a0625ff
Wooseong Kim <innocarpe@gmail.com>
2023-09-24 00:31:00 +0900

    refactor(app): 로그인과 관련된 처리를 전담할 useLogin 커스텀 훅 작성

---
+++ apps/react-world/src/hooks/useLogin.ts
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import type { LoginUserErrors } from '../apis/login/LoginService.types'; // 로그인 관련 타입으로 수정
+import LoginService from '../apis/login/LoginService'; // LoginService로 수정
+import { saveTokenToCookie } from '@utils/jwtUtils';
+
+export type LoginStatus = 'idle' | 'loggingIn' | 'success' | 'failed';
+
+const useLogin = () => {
+  const [loginError, setLoginError] = useState<LoginUserErrors | null>(null);
+  const [loginStatus, setLoginStatus] = useState<LoginStatus>('idle');
+
+  const handleLogin = async (data: { email: string; password: string }) => {
+    try {
+      setLoginStatus('loggingIn');
+      const response = await LoginService.loginUser(data);
+      // JWT 토큰을 쿠키에 저장
+      if (response && response.user && response.user.token) {
+        saveTokenToCookie(response.user.token);
+      }
+      setLoginStatus('success');
+      console.log('login suceess');
+      return response;
+    } catch (error) {
+      if (error && typeof error === 'object' && 'errors' in error) {
+        setLoginError(error.errors as LoginUserErrors);
+        console.error('LoginUserErrors: ', error.errors);
+      } else {
+        console.error('An unexpected error occurred:', error);
+      }
+      setLoginStatus('failed');
+      return null;
+    }
+  };
+
+  return {
+    loginError,
+    loginStatus,
+    handleLogin,
+  };
+};
+
+export default useLogin;
