9eb220e0d6d7a181fbdc2f4026b799854adcfd63
Wooseong Kim <innocarpe@gmail.com>
2023-09-24 00:24:47 +0900

    refactor(app): StyledLoginButton 작성

--- apps/react-world/src/components/login/LoginForm.styled.ts
+++ apps/react-world/src/components/login/LoginForm.styled.ts
@@ -6,3 +6,12 @@ export const LoginErrorMessage = styled.p`
   margin-left: 5px;
   font-size: 14px;
 `;
+
+export const StyledLoginButton = styled.button<{ disabled: boolean }>`
+  background-color: ${props => (props.disabled ? '#bbb' : '#5cb85c')};
+  border-color: ${props => (props.disabled ? '#bbb' : '#5cb85c')};
+  color: white;
+
+  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
+  padding: 10px 20px; // 기본 버튼 패딩
+`;
