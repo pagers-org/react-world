1284bbe6823e0b77fc7f29df7141ba641ee4f123
Wooseong Kim <innocarpe@gmail.com>
2023-09-24 00:15:08 +0900

    refactor(app): LoginForm.styled.ts 를 추가하고 LoginErrorMessage 이동

---
+++ apps/react-world/src/components/login/LoginForm.styled.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const LoginErrorMessage = styled.p`
+  color: red;
+  margin-top: 5px;
+  margin-left: 5px;
+  font-size: 14px;
+`;
--- apps/react-world/src/components/login/LoginForm.tsx
+++ apps/react-world/src/components/login/LoginForm.tsx
@@ -80,11 +80,4 @@ const LoginForm = (props: LoginFormProps) => {
   );
 };
 
-const LoginErrorMessage = styled.p`
-  color: red;
-  margin-top: 5px;
-  margin-left: 5px;
-  font-size: 14px;
-`;
-
 export default LoginForm;
