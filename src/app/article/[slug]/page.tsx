6e3fc1f0d64ce2e6499fb75a509e976cba4e90f0
ding-co <pangoons@naver.com>
2023-09-24 15:16:39 +0900

    update: Article detail page public page

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -1,29 +1,15 @@
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import ArticleDetailPageMain from '@/pageComponents/article/[slug]/ArticleDetailPageMain';
 
-import { COOKIE_ACCESS_TOKEN_KEY } from '@/constants/key';
-
 import { getArticle } from '@/api/articles';
 
 const getArticleDetail = async (slug: string) => {
-  const cookieStore = cookies();
-
-  const accessToken = cookieStore.get(COOKIE_ACCESS_TOKEN_KEY)?.value;
-
   if (!slug) {
     redirect('/');
   }
 
-  const res = await getArticle(
-    slug,
-    accessToken
-      ? {
-          Authorization: `Bearer ${accessToken}`,
-        }
-      : {},
-  );
+  const res = await getArticle(slug);
 
   if (res?.errors) {
     redirect('/');
