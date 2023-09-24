e2b03bd11584d1242ea4019182ae949e8edbc7d6
cham0287 <crtmt97@gmail.com>
2023-09-24 19:43:00 +0900

    Fix: 공통 api함수 localStorage 로직 제거

--- src/services/app.ts
+++ src/services/app.ts
@@ -8,22 +8,15 @@ interface ExecuteAPIProps {
 const API_ENDPINT = "https://api.realworld.io/api";
 
 export const executeAPI = async ({ method, body, url }: ExecuteAPIProps) => {
-  const token = localStorage.get("Authorization");
-  const authorization = `Bearer ${token}`;
-
   const promise_api = await fetch(`${API_ENDPINT}${url}`, {
     method,
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
-      // Authorization: "XXXXX.YYYYY.ZZZZZ",
     },
     body: JSON.stringify(body),
   });
 
   const response = await promise_api.json();
-  // if (!response.ok) {
-  //   console.error(response.message);
-  // }
 
   return response;
 };
