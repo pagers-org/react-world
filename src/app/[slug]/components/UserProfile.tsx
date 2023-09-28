3cfae650e87413d74419cea7a2022b5112779769
moseung <tmdah900@naver.com>
2023-09-28 17:39:03 +0900

    feat: 마이페이지 유저 프로필 컴포넌트 추가

---
+++ src/app/[slug]/components/UserProfile.tsx
@@ -0,0 +1,56 @@
+import { ProfileResponseType } from '@/types/profile';
+import { usePathname, useRouter } from 'next/navigation';
+import { AddOutline, SettingsOutline } from 'react-ionicons';
+
+import { ParseGetLocalStorage } from '@/utils/browserStorage';
+
+interface UserPorfileProps {
+  userData: ProfileResponseType;
+}
+
+const UserProfile = ({ userData }: UserPorfileProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const pathOnlyUserName = pathname?.substring(2);
+  return (
+    <div className="user-info">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <div className="col-xs-12 col-md-10 offset-md-1 !flex !flex-col !items-center !ml-0">
+            <img src={userData.profile.image} className="user-img" />
+            <h4>{userData.profile.username}</h4>
+            <p>{userData.profile.bio}</p>
+          </div>
+          {pathOnlyUserName === userData.profile.username ? (
+            <div className="w-full justify-end flex">
+              <button className="btn btn-sm btn-outline-secondary action-btn !flex !items-center !w-fit">
+                <AddOutline
+                  cssClasses={'mr-2'}
+                  width={'10.5px'}
+                  height={'14px'}
+                />
+                <p className="!mb-0">{`Follow ${userData.profile.username}`}</p>
+              </button>
+            </div>
+          ) : (
+            <div className="w-full justify-end flex">
+              <button
+                onClick={() => router.push('/settings')}
+                className="btn btn-sm btn-outline-secondary action-btn !flex !items-center !w-fit"
+              >
+                <SettingsOutline
+                  cssClasses={'mr-2'}
+                  width={'10.5px'}
+                  height={'14px'}
+                />
+                <p className="!mb-0">Edit Profile Settings</p>
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
