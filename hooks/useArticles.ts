77d971849b264ab50b39def9b927726867080d3e
hyeon9782 <hyeon9782@gmail.com>
2023-09-29 11:38:34 +0900

    fix: useArticles 데이터가 적을 경우 생기는 에러 해결

--- hooks/useArticles.ts
+++ hooks/useArticles.ts
@@ -6,20 +6,20 @@ import useIntersectionObserver from './useIntersectionObserver';
 const useArticles = (ref: RefObject<HTMLElement>, tab: string) => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
     queryKey: ['articles', tab],
-    queryFn: ({ pageParam = 0 }) => {
+    queryFn: async ({ pageParam = 0 }) => {
       switch (tab) {
         case 'global':
-          return getArticlesAPI(pageParam);
+          return await getArticlesAPI(pageParam);
         case 'your':
           return fetch(`/api/articles/feed?page=${pageParam}`).then(res => res.json());
         default:
-          return getArticlesWithTagAPI(tab, pageParam);
+          return await getArticlesWithTagAPI(tab, pageParam);
       }
     },
     getNextPageParam: (lastPage, pages) => {
       const totalPage = Math.ceil(lastPage.articlesCount / 10);
       let currentPage = pages.length;
-      if (lastPage.articles === 0 || totalPage < pages.length) {
+      if (lastPage.articlesCount < 11 || totalPage < pages.length) {
         return undefined;
       }
 
