87fbd0a66c4df5e313bd5f63d55e53a8e8d0d5bb
hyeon9782 <hyeon9782@gmail.com>
2023-10-05 03:30:15 +0900

    feat: 프로필 정보 가져오기 기능 구현

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -2,30 +2,33 @@
 
 import ArticleList from '@/components/article/ArticleList';
 import { SettingIcon } from '@/composables/icons';
-import useUserStore from '@/stores/useUserStore';
 import { container } from '@/styles/common.css';
 import { settingButton, userBlock, userImage, userInfo, userName } from '@/styles/profile.css';
-import { User } from '@/types';
+import { useQuery } from '@tanstack/react-query';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const ArticleTab = dynamic(() => import('@/components/article/ArticleTab'), { ssr: false });
+type Props = {
+  params: { username: string };
+};
+const ProfilePage = ({ params: { username } }: Props) => {
+  const { data: profile } = useQuery({
+    queryKey: ['profile', username],
+    queryFn: () => fetch(`/api/profiles/${username}`).then(res => res.json()),
+    enabled: !!username,
+    select: res => res.response.profile,
+  });
 
-const ProfilePage = () => {
-  const { username } = useUserStore() as User;
+  console.log(profile);
+
   return (
     <section>
       <div className={userBlock}>
         <div className={userInfo}>
-          <Image
-            src={'https://api.realworld.io/images/smiley-cyrus.jpeg'}
-            alt="Profile"
-            width={100}
-            height={100}
-            className={userImage}
-          />
-          <div className={userName}>{username}</div>
+          <Image src={profile.image} alt="Profile" width={100} height={100} className={userImage} />
+          <div className={userName}>{profile.username}</div>
           <Link href="/settings" className={settingButton}>
             <SettingIcon />
             &nbsp; Edit Profile Settings
--- app/api/profiles/[username]/route.ts
+++ app/api/profiles/[username]/route.ts
@@ -1,6 +1,29 @@
 import { http } from '@/utils/http';
 import { NextRequest, NextResponse } from 'next/server';
 
+async function GET(req: NextRequest, route: { params: { username: string } }) {
+  const username = route.params.username;
+  console.log('route User Name' + username);
+  console.log(username.replace('@', ''));
+
+  const token = req.cookies.get('token')?.value || '';
+
+  try {
+    const response = await http.get(`/profiles/${username.replace('@', '')}`, {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    });
+    console.log('route');
+
+    console.log(response);
+
+    return NextResponse.json({ message: 'Get a Profile Success', response });
+  } catch (err) {
+    return NextResponse.json({ message: 'Get a Profile Fail', err });
+  }
+}
+
 // Follow a user
 async function POST(req: NextRequest, route: { params: { username: string } }) {
   const username = route.params.username;
@@ -37,4 +60,4 @@ async function DELETE(req: NextRequest, route: { params: { username: string } })
   }
 }
 
-export { POST, DELETE };
+export { GET, POST, DELETE };
