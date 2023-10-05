32f10cafdb1c865a99b02baf4abe531b0e84f9f2
ding-co <pangoons@naver.com>
2023-10-05 14:30:45 +0900

    update: Article detail page conditional by login user for knowing follow author

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -1,13 +1,24 @@
 import { FeedResponse } from '@/types/api/articles';
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import ArticleDetailPageMain from '@/pageComponents/article/[slug]/ArticleDetailPageMain';
+
+import { COOKIE_ACCESS_TOKEN_KEY } from '@/constants/key';
 
 import { getArticle } from '@/api/articles';
 
 const getArticleDetail = async (slug: string) => {
   try {
-    const res = (await getArticle({ slug })) as FeedResponse;
+    const cookieStore = cookies();
+    const accessToken = cookieStore.get(COOKIE_ACCESS_TOKEN_KEY)?.value;
+
+    const res = (await getArticle({
+      slug,
+      headers: {
+        ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
+      },
+    })) as FeedResponse;
     return res.article;
   } catch (e) {
     redirect('/');
