b65a2b23874bd1e0fb3ca535d5f29599f133bdd2
Wooseong Kim <innocarpe@gmail.com>
2023-10-05 20:51:18 +0900

    refactor(app): NAV_ITEMS 를 constants 폴더 내의 파일로 분리

--- apps/react-world/src/types/NavItemModel.ts
+++ apps/react-world/src/types/NavItemModel.ts
@@ -1,12 +1,6 @@
 export type NavItemType = 'home' | 'login' | 'register';
 
-type NavItemModel = {
+export type NavItemModel = {
   name: NavItemType;
   path: string;
 };
-
-export const NAV_ITEMS: NavItemModel[] = [
-  { name: 'home', path: '/' },
-  { name: 'login', path: '/login' },
-  { name: 'register', path: '/register' },
-];
