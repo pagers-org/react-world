fc101d6952511939b51e90d4e9aa34e648a85931
root <root@DESKTOP-J22D89D>
2023-10-09 17:33:56 +0900

    feat: Sidebar component

---
+++ 2taesung/src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <p>Popular Tags</p>
+
+      <div className="tag-list">
+        <a href="" className="tag-pill tag-default">
+          programming
+        </a>
+        <a href="" className="tag-pill tag-default">
+          javascript
+        </a>
+        <a href="" className="tag-pill tag-default">
+          emberjs
+        </a>
+        <a href="" className="tag-pill tag-default">
+          angularjs
+        </a>
+        <a href="" className="tag-pill tag-default">
+          react
+        </a>
+        <a href="" className="tag-pill tag-default">
+          mean
+        </a>
+        <a href="" className="tag-pill tag-default">
+          node
+        </a>
+        <a href="" className="tag-pill tag-default">
+          rails
+        </a>
+      </div>
+    </div>
+  );
+}
